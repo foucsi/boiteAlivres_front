aa46fc3ecaacfd35cfe7ae0bb9fe978361a3e24a
Foucsi <julien.foucartreact@gmail.com>
2024-06-01 08:00:36 +0200

    add init project

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,8 +2,18 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" />
+    <Stack
+        screenOptions={{
+            headerStyle: {
+                backgroundColor: '#f4511e',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                fontWeight: 'bold',
+            },
+        }}>
+        <Stack.Screen name="index" />
+        <Stack.Screen name="login" />
     </Stack>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,5 @@
 import { Text, View } from "react-native";
+import {Link} from "expo-router";
 
 export default function Index() {
   return (
@@ -9,7 +10,8 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Welcome screen</Text>
+        <Text>Welcome screen</Text>
+        <Link href="/login">Go to home screen</Link>
     </View>
   );
 }
