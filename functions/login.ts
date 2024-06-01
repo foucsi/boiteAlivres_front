54fc07ac5d07f29a1091824c1ddb51659243d864
Foucsi <julien.foucartreact@gmail.com>
2024-06-01 21:17:13 +0200

    add try catch

--- functions/login.ts
+++ functions/login.ts
@@ -1,3 +1,29 @@
-export const login = async()=>{
+import {URL_LOGIN} from "@/constants/Url";
+
+interface User {
+    email: string;
+    password: string;
+
+}
+
+export const login = async(user:User)=>{
+    try{
+        const response = await fetch(URL_LOGIN, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+        const data = await response.json()
+        if(data.result){
+            return {success:true, user:data.user, token:data.token}
+        }else{
+            return {success:false, message:data.message}
+        }
+    }catch(err){
+        console.log(err)
+        return {success:false, error:err}
+    }
 
 }
--- functions/register.ts
+++ functions/register.ts
@@ -7,17 +7,23 @@ interface UserData{
 }
 
 export const register = async (user : UserData) => {
-    const response = await fetch(URL_REGISTER, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(user),
-    })
-    const data = await response.json()
-    if(data.result){
-        return {success: true, message: data.error || data.message}
-    }else{
-        return {success: false, message: data.error || data.message}
+    try{
+        const response = await fetch(URL_REGISTER, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(user),
+        })
+        const data = await response.json()
+        if(data.result){
+            return {success: true, message: data.error || data.message}
+        }else{
+            return {success: false, message: data.error || data.message}
+        }
+    }catch(err){
+        console.log(err)
+        return {success: false, error: err}
     }
+
 }
