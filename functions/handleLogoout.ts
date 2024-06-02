d547c67abb96a19bbaca886f551a94ac1663dff9
Foucsi <julien.foucartreact@gmail.com>
2024-06-02 11:12:49 +0200

    add handleLogout function

---
+++ functions/handleLogoout.ts
@@ -0,0 +1,8 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import {logout} from "@/redux/users";
+
+export const handleLogout = async (dispatch:any, router:any)=>{
+    dispatch(logout())
+    await AsyncStorage.removeItem('token')
+    router.navigate("/login")
+}
