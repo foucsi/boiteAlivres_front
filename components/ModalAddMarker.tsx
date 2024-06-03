4ae94215c93ac5a7d5aa3974793036f539dce20e
Foucsi <julien.foucartreact@gmail.com>
2024-06-03 16:16:11 +0200

    add e.nativeEvent

--- components/ModalAddMarker.tsx
+++ components/ModalAddMarker.tsx
@@ -1,84 +1,25 @@
-import {View, Text, StyleSheet,TouchableOpacity} from 'react-native'
+import {View, Text, StyleSheet, Button} from 'react-native'
 import React from 'react'
 import {useSelector} from "react-redux";
-import { AntDesign } from '@expo/vector-icons';
+import Modal from "react-native-modal";
 
 // @ts-ignore
 export default function ModalAddMarker({modalVisible, setModalVisible}) {
     const user = useSelector((state: any) => state.user.value)
     return (
-        <View style={styles.container}>
-            <Modal
-                animationType="slide"
-                transparent={true}
-                visible={modalVisible}
-                onRequestClose={() => {
-                    setModalVisible(!modalVisible);
-                }}
-            >
-                <View style={styles.modalContainer}>
-                    <View style={styles.modalContent}>
-                        <Text style={styles.modalText}>
-                            Merci Beaucoup !
-                            Votre contribution a été soumise avec succès et est en attente de modération.
-                            Un modérateur validera votre ajout sous 24 heures.
-                        </Text>
-                        <TouchableOpacity
-                            style={styles.closeButton}
-                            onPress={() => setModalVisible(false)}
-                        >
-                            <Text style={styles.closeButtonText}>Fermer</Text>
-                        </TouchableOpacity>
-                    </View>
+        <View style={{ flex: 1 }}>
+            <Modal isVisible={modalVisible}>
+                <View style={{ flex: 1 }}>
+                    <Text>Hello!</Text>
+                    <Button title="Hide modal" onPress={()=>setModalVisible(false)} />
                 </View>
             </Modal>
         </View>
     );
 }
 
-
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    button: {
-        backgroundColor: '#2196F3',
-        borderRadius: 10,
-        padding: 10,
-        marginBottom: 20,
-    },
-    buttonText: {
-        color: 'white',
-        fontWeight: 'bold',
-        textAlign: 'center',
-    },
-    modalContainer: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'rgba(0, 0, 0, 0.5)',
-    },
-    modalContent: {
-        backgroundColor: 'white',
-        borderRadius: 10,
-        padding: 20,
-        alignItems: 'center',
-    },
-    modalText: {
-        marginBottom: 15,
-        textAlign: 'center',
-    },
-    closeButton: {
-        backgroundColor: '#E91E63',
-        borderRadius: 10,
-        padding: 10,
-        marginTop: 20,
-    },
-    closeButtonText: {
-        color: 'white',
-        fontWeight: 'bold',
-        textAlign: 'center',
-    },
-});
+
+})
+
+
