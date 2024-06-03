7f41a135207eca206dc11ccb590d5bbacfe35ce4
Foucsi <julien.foucartreact@gmail.com>
2024-06-03 11:09:53 +0200

    add e.nativeEvent

--- hooks/useAddMarkerInDb.ts
+++ hooks/useAddMarkerInDb.ts
@@ -1,20 +1,22 @@
 import {useState} from "react";
 import {addMarkerInDb} from "@/functions/addMarkerInDb";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+import {addBookSpace} from "@/redux/bookSpaces"
 
 export const useAddMarkerInDb = () => {
     const [markerDb, setMarkerDb] = useState([]);
-
+    const dispatch = useDispatch();
     // @ts-ignore
     const user = useSelector((state) => state.user.value);
     const addMarker = async(e:any)=>{
         const {latitude, longitude} = e.nativeEvent.coordinate
         e.persist()
-        const result = await addMarkerInDb(user.uniqueId, latitude, longitude, "test")
+        const result = await addMarkerInDb(user.uniqueId, latitude, longitude, "Boite A Livres")
         console.log("NativeEvent: ", e.nativeEvent.coordinate)
          if(result.success){
-             console.log("Add marker successfully: ", result.data)
+             // console.log("Add marker successfully: ", result.data)
               setMarkerDb(result.data)
+              dispatch(addBookSpace(result.data))
          }else{
                 console.log("error add marker: ", result)
          }
