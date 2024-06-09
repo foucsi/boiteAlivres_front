7aa77fb42642e37f16086215658783d5489e728f
Foucsi <julien.foucartreact@gmail.com>
2024-06-09 18:33:48 +0200

    add modal comment component

--- components/ModalComment.tsx
+++ components/ModalComment.tsx
@@ -1,5 +1,6 @@
-import {View, Text, Modal, TouchableOpacity, StyleSheet} from 'react-native'
+import {View, Text, Modal, TouchableOpacity, StyleSheet, TextInput} from 'react-native'
 import React from 'react'
+import { AntDesign } from '@expo/vector-icons';
 
 // @ts-ignore
 export default function ModalComment({setModalCommentVisible}) {
@@ -10,9 +11,21 @@ export default function ModalComment({setModalCommentVisible}) {
         <Modal animationType="slide" transparent={true} >
             <View style={styles.modal}>
                 <View style={styles.modalContainer}>
-                    <TouchableOpacity onPress={closeModal}>
-                        <Text>Close</Text>
-                    </TouchableOpacity>
+                    <View style={{padding:5}}>
+                        <TouchableOpacity>
+                            <AntDesign name="closecircleo" size={24} color="#294C60" onPress={closeModal}/>
+                        </TouchableOpacity>
+                    </View>
+                    <View>
+                        <TextInput multiline={true} style={styles.input} placeholder="Ajouter un commentaire"/>
+                    </View>
+                    <View>
+                        <TouchableOpacity
+                            style={styles.button}
+                        >
+                            <Text style={styles.buttonText}>Envoyer</Text>
+                        </TouchableOpacity>
+                    </View>
                 </View>
             </View>
         </Modal>
@@ -26,11 +39,46 @@ const styles = StyleSheet.create({
         justifyContent:"center",
     },
     modalContainer:{
-        alignItems:"center",
-        justifyContent:"center",
+        padding: 10,
         height:200,
         width:300,
         backgroundColor:"white",
         borderRadius: 10,
-    }
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4.84,
+        elevation: 5,
+    },
+    input:{
+        width: '100%',
+        height: 70,
+        borderStyle:"solid",
+        borderColor: '#294C60',
+        borderWidth: 1,
+        borderRadius: 10,
+        padding: 10,
+        marginTop: 10,
+    },
+    button: {
+        marginTop: 10,
+        backgroundColor: '#294C60',
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        borderRadius: 20,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.8,
+        shadowRadius: 2,
+        elevation: 5,
+    },
+    buttonText: {
+        color: '#FFF',
+        fontSize: 18,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
 })
