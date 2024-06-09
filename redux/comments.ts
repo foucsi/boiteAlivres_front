46bc0350d724caaadebc0b01e97ff2cafb5ee170
Foucsi <julien.foucartreact@gmail.com>
2024-06-09 07:41:52 +0200

    add reducer comments

--- redux/comments.ts
+++ redux/comments.ts
@@ -5,3 +5,20 @@ const initialState = {
         comments:[],
     }
 }
+
+export const commentsSlice = createSlice({
+    name: "comment",
+    initialState,
+    reducers:{
+        addComment: (state, action) => {
+            // @ts-ignore
+            state.value.comments.push(action.payload);
+        },
+        removeComment: (state) => {
+            state.value.comments = []
+        }
+    }
+})
+
+export const {addComment, removeComment} = commentsSlice.actions;
+export default commentsSlice.reducer;
