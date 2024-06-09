cf6f969f4b79b58cc636df0bf6574837973b11de
Foucsi <julien.foucartreact@gmail.com>
2024-06-09 08:57:12 +0200

    add exports.updateDescription

---
+++ functions/updateDescriptionBookPlace.ts
@@ -0,0 +1,20 @@
+export const updateDescriptionBookPlace = async (bookPlaceId:any, description:string) => {
+    const url = `http://localhost:3000/bookPlaces/${bookPlaceId}`
+    try{
+        const response = await fetch(url,{
+            method: 'PUT',
+            headers:{
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({description:description})
+        })
+        const data = await response.json()
+        if(data.result){
+            return {success:true}
+        }else {
+            return {success:false, error: data.error}
+        }
+    }catch(err){
+        return {success:false, error: err}
+    }
+}
