a94bebd81bed77bd29464520f4c84e84ece40983
Foucsi <julien.foucartreact@gmail.com>
2024-06-09 17:50:27 +0200

    add modal comment component

---
+++ components/ModalComment.tsx
@@ -0,0 +1,10 @@
+import {View, Text,Modal} from 'react-native'
+import React from 'react'
+
+export default function ModalComment() {
+    return (
+        <Modal>
+            <Text>ModalComment</Text>
+        </Modal>
+    )
+}
