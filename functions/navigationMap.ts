b54eab8315a364d9c8feb344a54da689a486f937
Foucsi <julien.foucartreact@gmail.com>
2024-06-10 16:38:52 +0200

    delete comment

--- functions/navigationMap.ts
+++ functions/navigationMap.ts
@@ -2,5 +2,11 @@ import {Linking} from "react-native";
 
 export const handleLinking = (latitude:number, longitude:number) => {
     // Linking.openURL("https://www.google.com/maps/dir/?api=1&destination=" + marker.latitude + "," + marker.longitude")
-    Linking.openURL(`maps://app?daddr=${latitude},${longitude}&dirflg=d`);
+    {/*
+        dirflg : Spécifie le mode de transport pour l'itinéraire. Les options incluent :
+            d pour la conduite (driving),
+            w pour la marche (walking),
+            r pour les transports en commun (transit).
+    */}
+    Linking.openURL(`maps://app?daddr=${latitude},${longitude}&dirflg=w`);
 };
