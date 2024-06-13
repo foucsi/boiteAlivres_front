63f61db6c373b92c9c117c576b01eeeb770a0b6c
Foucsi <julien.foucartreact@gmail.com>
2024-06-13 17:19:57 +0200

    add throw new error

--- functions/getAllBookPlaces.ts
+++ functions/getAllBookPlaces.ts
@@ -3,6 +3,9 @@ import {URL_GET_ALL_BOOK_PLACES} from "@/constants/Url";
 export const getAllBookPlaces = async () => {
     try{
         const response = await fetch(URL_GET_ALL_BOOK_PLACES);
+        if(!response.ok){
+            throw new Error(`HTTP error! status: ${response.status} ${response.statusText}`);
+        }
         const data = await response.json();
         if(data.result){
             return {success: true, bookPlaces: data.bookPlaces}
