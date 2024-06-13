0f21b0ab8aad61d9608ec2c1a03f109c829f5a19
Foucsi <julien.foucartreact@gmail.com>
2024-06-13 17:13:05 +0200

    add useMemo()

--- functions/getAllCommentsByBookPlace.ts
+++ functions/getAllCommentsByBookPlace.ts
@@ -2,7 +2,6 @@ export const getAllCommentsByBookPlace = async(bookPlaceId:string)=>{
     const url = `http://localhost:3000/comments/getAllCommentsByBookPlace/${bookPlaceId}`
     try{
         const response = await fetch(url)
-        // comment below if there is problem with response.ok
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status} ${response.statusText}`);
         }
