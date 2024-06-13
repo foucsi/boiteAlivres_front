0d093f10e9532413f89f04916ad5d1fc9da9be6d
Foucsi <julien.foucartreact@gmail.com>
2024-06-13 09:22:37 +0200

    add useMemo()

--- components/ModalBookPlaces.tsx
+++ components/ModalBookPlaces.tsx
@@ -37,7 +37,7 @@ import ModalComment from "@/components/ModalComment";
 import {getAllBookPlaces} from "@/functions/getAllBookPlaces";
 
 // @ts-ignore
-export default function ModalBookPlaces({modalVisible, setModalVisible, selectedMarker, setSelectedMarker, navigation}) {
+export default function ModalBookPlaces({modalVisible, setModalVisible, selectedMarker, setSelectedMarker}) {
     const momentDate = moment(selectedMarker.date).format('DD/MM/YYYY')
     const {comments, error, loading} = useGetAllCommentsByBookPlace(selectedMarker.id)
 
