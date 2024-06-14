ab756559f3559a596fc7fccb91fe534ba0e57251
Foucsi <julien.foucartreact@gmail.com>
2024-06-14 18:33:47 +0200

    add serverConfig

--- functions/serverConfig.ts
+++ functions/serverConfig.ts
@@ -1,4 +1,4 @@
-const SERVER_URL_PRODUCTION = process.env.NODE_ENV || 'http://192.168.140.132:3000';
+const SERVER_URL_PRODUCTION = process.env.NODE_ENV || 'http://192.168.1.38:3000';
 const SERVER_URL_DEVELOPMENT = 'http://localhost:3000';
 
 export const serverConfig = () => {
