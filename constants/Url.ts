b641d5c980676f1971662365d8cb62886448ef4e
Foucsi <julien.foucartreact@gmail.com>
2024-06-14 13:12:05 +0200

    function sharing expo

--- constants/Url.ts
+++ constants/Url.ts
@@ -1,9 +1,11 @@
-/* ---------------- URL ROUTES USERS ---------------- */
+import {serverConfig} from "@/functions/serverConfig";
 
+/* ---------------- URL ROUTES USERS ---------------- */
+const baseUrl = serverConfig()
 
 // URL for LOGIN and REGISTER Users
-export const URL_LOGIN :string = "http://localhost:3000/users/login"
-export const URL_REGISTER:string = "http://localhost:3000/users/register"
+export const URL_LOGIN :string = `${baseUrl}/users/login`
+export const URL_REGISTER:string = `${baseUrl}/users/register`
 
 // URL for get AllBookPlaces
 export const URL_GET_ALL_BOOK_PLACES:string = "http://localhost:3000/bookPlaces/getAllBookPlaces"
--- functions/serverConfig.ts
+++ functions/serverConfig.ts
@@ -1,4 +1,4 @@
-const SERVER_URL_PRODUCTION = process.env.PRODUCTION_URL || 'http://192.168.140.132:3000';
+const SERVER_URL_PRODUCTION = process.env.NODE_ENV || 'http://192.168.140.132:3000';
 const SERVER_URL_DEVELOPMENT = 'http://localhost:3000';
 
 export const serverConfig = () => {
