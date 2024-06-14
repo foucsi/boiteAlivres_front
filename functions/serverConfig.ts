a59471286d3a3621fba2153608191bcd65f2d95d
Foucsi <julien.foucartreact@gmail.com>
2024-06-14 11:28:35 +0200

    function sharing expo

--- functions/serverConfig.ts
+++ functions/serverConfig.ts
@@ -1,10 +1,7 @@
-export const serverConfig= ()=>{
-    // detect if we are in production or development
-    const isProduction = process.env.NODE_ENV === 'production';
-    if(isProduction){
-        return "http://192.168.140.132:3000"
-    }else {
-        return "http://localhost:3000"
-    }
+const SERVER_URL_PRODUCTION = process.env.PRODUCTION_URL || 'http://192.168.140.132:3000';
+const SERVER_URL_DEVELOPMENT = 'http://localhost:3000';
 
+export const serverConfig = () => {
+    const isProduction = process.env.NODE_ENV === 'production';
+    return isProduction ? SERVER_URL_PRODUCTION : SERVER_URL_DEVELOPMENT;
 }
