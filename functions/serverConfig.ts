976620bc1ad9649b049a15dd81cfa0d701de0e82
Foucsi <julien.foucartreact@gmail.com>
2024-06-14 19:27:59 +0200

    add serverConfig

--- functions/serverConfig.ts
+++ functions/serverConfig.ts
@@ -1,8 +1,11 @@
+import {NODE_ENV} from "@env";
+
 const SERVER_URL_PRODUCTION = 'http://192.168.1.38:3000'; // Adresse de production
 const SERVER_URL_DEVELOPMENT = 'http://localhost:3000'; // Adresse de développement local
 
 export const serverConfig = () => {
-    if(process.env.REACT_APP_NODE_ENV === 'production'){
+    console.log('process.env.NODE_ENV', NODE_ENV)
+    if(NODE_ENV === 'production'){
         return SERVER_URL_PRODUCTION
     }else{
         return SERVER_URL_DEVELOPMENT
