63a81fde600784f485b8aa345cc362f5f1343d4e
Foucsi <julien.foucartreact@gmail.com>
2024-06-14 18:48:43 +0200

    add serverConfig

--- functions/serverConfig.ts
+++ functions/serverConfig.ts
@@ -1,7 +1,10 @@
-const SERVER_URL_PRODUCTION = process.env.NODE_ENV || 'http://192.168.1.38:3000';
-const SERVER_URL_DEVELOPMENT = 'http://localhost:3000';
+const SERVER_URL_PRODUCTION = 'http://192.168.1.38:3000'; // Adresse de production
+const SERVER_URL_DEVELOPMENT = 'http://localhost:3000'; // Adresse de développement local
 
 export const serverConfig = () => {
-    const isProduction = process.env.NODE_ENV === 'production';
-    return isProduction ? SERVER_URL_PRODUCTION : SERVER_URL_DEVELOPMENT;
+    if(process.env.NODE_ENV === 'production'){
+        return SERVER_URL_PRODUCTION
+    }else{
+        return SERVER_URL_DEVELOPMENT
+    }
 }
