8c22ab24d85dd5612c0993640619dd5807263932
Foucsi <julien.foucartreact@gmail.com>
2024-06-16 09:28:48 +0200

    add modalPremium componenet

---
+++ components/ModalPremium.tsx
@@ -0,0 +1,10 @@
+import {View, Text} from 'react-native'
+import React from 'react'
+
+export default function ModalPremium() {
+    return (
+        <View>
+            <Text>ModalPremium</Text>
+        </View>
+    )
+}
