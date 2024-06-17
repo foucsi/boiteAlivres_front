5c43da652eca844625bbdf07a40232a85dfc3604
Foucsi <julien.foucartreact@gmail.com>
2024-06-17 09:35:41 +0200

    update design modalPremium

--- components/ModalPremium.tsx
+++ components/ModalPremium.tsx
@@ -1,15 +1,41 @@
-import {View, Text, Modal, TouchableOpacity} from 'react-native'
+import {View, Text, Modal, TouchableOpacity, StyleSheet} from 'react-native'
 import React from 'react'
 
 // @ts-ignore
 export default function ModalPremium({setModalPremiumIsVisible}) {
     return (
         <Modal animationType="slide" transparent={true}>
-            <View>
-                <TouchableOpacity onPress={()=>setModalPremiumIsVisible(false)}>
-                    <Text>Close Modal</Text>
-                </TouchableOpacity>
+            <View style={styles.modal}>
+                <View style={styles.modalContainer}>
+                    <TouchableOpacity onPress={()=>setModalPremiumIsVisible(false)}>
+                        <Text>Close Modal</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
         </Modal>
     )
 }
+
+const styles = StyleSheet.create({
+    modal:{
+        flex:1,
+        alignItems:"center",
+        justifyContent:"center",
+        backgroundColor: 'rgba(0,0,0,0.5)',
+    },
+    modalContainer:{
+        padding: 10,
+        height:200,
+        width:300,
+        backgroundColor:"white",
+        borderRadius: 10,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4.84,
+        elevation: 5,
+    },
+})
