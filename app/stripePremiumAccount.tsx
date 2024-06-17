dbbaf2d961ffd0113dfe4c8b7c9152960e0f37d5
Foucsi <julien.foucartreact@gmail.com>
2024-06-17 09:56:06 +0200

    add componenet stripe

--- app/stripePremiumAccount.tsx
+++ app/stripePremiumAccount.tsx
@@ -1,10 +1,18 @@
-import {View, Text} from 'react-native'
+import {View, Text, StyleSheet} from 'react-native'
 import React from 'react'
 
 export default function StripePremiumAccount() {
     return (
-        <View>
+        <View style={styles.container}>
             <Text>StripePremiumAccount</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent:"center",
+        alignItems:"center"
+    }
+})
