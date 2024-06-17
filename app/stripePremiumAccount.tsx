96fc60360431cd63cb6299ab30ff5857c435792f
Foucsi <julien.foucartreact@gmail.com>
2024-06-17 09:52:04 +0200

    add componenet stripe

---
+++ app/stripePremiumAccount.tsx
@@ -0,0 +1,10 @@
+import {View, Text} from 'react-native'
+import React from 'react'
+
+export default function StripePremiumAccount() {
+    return (
+        <View>
+            <Text>StripePremiumAccount</Text>
+        </View>
+    )
+}
