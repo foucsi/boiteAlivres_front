daedc2fea6a60a12b638b4cfb27f909079db075f
Foucsi <julien.foucartreact@gmail.com>
2024-06-17 09:52:59 +0200

    add componenet stripe

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -48,6 +48,7 @@ export default function RootLayout() {
                 <Stack.Screen name="register" />
                 <Stack.Screen name="welcome" />
                 <Stack.Screen name="mapScreen" />
+                <Stack.Screen name="stripePremiumAccount" />
             </Stack>
           </PersistGate>
       </Provider>
