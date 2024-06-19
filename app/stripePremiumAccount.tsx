104bd5106e3e08449f48c06afe8b44c766eb23f6
Foucsi <julien.foucartreact@gmail.com>
2024-06-19 10:21:27 +0200

    add stripe json

--- app/stripePremiumAccount.tsx
+++ app/stripePremiumAccount.tsx
@@ -1,10 +1,16 @@
-import {View, Text, StyleSheet} from 'react-native'
+import {View, Text, StyleSheet, TextInput} from 'react-native'
 import React from 'react'
+import { AntDesign } from '@expo/vector-icons';
 
 export default function StripePremiumAccount() {
     return (
         <View style={styles.container}>
-            <Text>StripePremiumAccount</Text>
+            <AntDesign name="creditcard" size={96} color="black" />
+            <View>
+                <TextInput style={styles.input} placeholder="Card Number" />
+                <TextInput style={styles.input} placeholder="MM/YY" />
+                <TextInput style={styles.input} placeholder="CVC" />
+            </View>
         </View>
     )
 }
@@ -14,5 +20,13 @@ const styles = StyleSheet.create({
         flex:1,
         justifyContent:"center",
         alignItems:"center"
+    },
+    input:{
+        borderWidth:1,
+        borderColor:"black",
+        borderRadius:5,
+        padding:10,
+        margin:10,
+        width:300
     }
 })
