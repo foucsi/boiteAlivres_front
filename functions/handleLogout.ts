7aad51d4cdbe2aca005b117c860ce34afaaf2b49
Foucsi <julien.foucartreact@gmail.com>
2024-06-20 18:24:16 +0200

    add logout settings screen

--- functions/handleLogout.ts
+++ functions/handleLogout.ts
@@ -1,8 +1,8 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import {logout} from "@/redux/users";
 
-export const handleLogout = async (dispatch:any, router:any)=>{
+export const handleLogout = async (dispatch:any, router:any, path:any)=>{
     dispatch(logout())
     await AsyncStorage.removeItem('token')
-    router.navigate("/")
+    router.navigate(path)
 }
