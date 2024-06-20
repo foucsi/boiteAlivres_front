fcf538e2e3122bb93cdbe094058077c591c35cda
Foucsi <julien.foucartreact@gmail.com>
2024-06-20 11:53:31 +0200

    add tab navigator

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -7,7 +7,16 @@ export default function TabLayout() {
             <Tabs.Screen
                 name="index"
                 options={{
+                    headerShown: false,
                     title: 'Home',
+                    tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
+                }}
+            />
+            <Tabs.Screen
+                name="settings"
+                options={{
+                    headerShown: false,
+                    title: 'Settings',
                     tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
                 }}
             />
