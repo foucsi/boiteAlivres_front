4f6765756d0ae977a7cb2af5523764fa06c9bfa8
Foucsi <julien.foucartreact@gmail.com>
2024-06-20 18:36:56 +0200

    add logout settings screen

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,10 +1,11 @@
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native'
+import {View, Text, StyleSheet, TouchableOpacity, Image} from 'react-native'
 import React from 'react'
 import {router} from 'expo-router'
 
 //IMPORT FUNCTION handleLogout
 import {handleLogout} from "@/functions/handleLogout";
 import {useDispatch, useSelector} from "react-redux";
+import {maxHeight} from "@mui/system";
 
 
 //username, email, photo, premium
@@ -15,12 +16,18 @@ export default function Settings() {
     const users = useSelector(state => state.user.value)
     return (
         <View style={styles.container}>
-            <Text>{users.username}</Text>
-            <Text>{users.email}</Text>
+            <View style={styles.settingsContainer}>
+                <Text style={styles.mainTitle}>PARAMÈTRES</Text>
+                <Image source={{uri: users.photo}} style={{width: 100, height: 100}}/>
+                <Text style={styles.text}>{users.username}</Text>
+                <Text style={styles.text}>{users.email}</Text>
+            </View>
+            <View style={styles.containerLogout}>
+                <TouchableOpacity style={styles.btnLogout} onPress={()=>handleLogout(dispatch, router, "/login")}>
+                    <Text>Déconnecter</Text>
+                </TouchableOpacity>
+            </View>
 
-            <TouchableOpacity onPress={()=>handleLogout(dispatch, router, "/login")}>
-                <Text>Logout</Text>
-            </TouchableOpacity>
         </View>
     )
 }
@@ -28,7 +35,35 @@ export default function Settings() {
 const styles = StyleSheet.create({
     container:{
         flex:1,
-        justifyContent:'center',
-        alignItems:'center'
+        backgroundColor: 'white'
     },
+    mainTitle:{
+        fontSize: 38,
+        textAlign: 'center',
+        marginTop: 20,
+    },
+    text:{
+        fontSize: 20,
+        marginTop: 20,
+    },
+    settingsContainer:{
+        padding: 20,
+        width:"100%",
+        height:400,
+        justifyContent: 'center',
+    },
+    containerLogout:{
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        marginTop: 20,
+        height:"40%",
+    },
+    btnLogout:{
+        backgroundColor: 'red',
+        padding: 10,
+        borderRadius: 5,
+        width: "50%",
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
 })
