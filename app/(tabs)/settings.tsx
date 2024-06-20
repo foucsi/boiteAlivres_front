f13bfe76303d7e76b4ace2e64c3b02be3e90252e
Foucsi <julien.foucartreact@gmail.com>
2024-06-20 17:21:46 +0200

    add logout settings screen

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,10 +1,19 @@
-import {View, Text, StyleSheet} from 'react-native'
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native'
 import React from 'react'
+import {router} from 'expo-router'
+
+//IMPORT FUNCTION
+import {handleLogout} from "@/functions/handleLogout";
+import {useDispatch} from "react-redux";
 
 export default function Settings() {
+    const dispatch = useDispatch()
     return (
         <View style={styles.container}>
             <Text>Settings</Text>
+            <TouchableOpacity onPress={()=>handleLogout(dispatch, router)}>
+                <Text>Logout</Text>
+            </TouchableOpacity>
         </View>
     )
 }
