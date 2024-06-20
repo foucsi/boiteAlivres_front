329956c26285dcc65af3b00613e631575620a634
Foucsi <julien.foucartreact@gmail.com>
2024-06-20 19:13:41 +0200

    add logout settings screen

--- redux/users.ts
+++ redux/users.ts
@@ -29,7 +29,7 @@ export const userSlice = createSlice({
             state.value.username = null;
             state.value.email = null;
             state.value.uniqueId = null;
-            state.value.premium = null;
+            // state.value.premium = null;
             // state.value.photo =
             //   "https://media.istockphoto.com/id/1300845620/fr/vectoriel/appartement-dic%C3%B4ne-dutilisateur-isol%C3%A9-sur-le-fond-blanc-symbole-utilisateur.jpg?b=1&s=170667a&w=0&k=20&c=HEO2nP4_uEAn0_JzVTU6_Y5hyn-qHxyCrWWTirBvScs=";
         },
