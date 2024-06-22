f1e71eca18fb29dd3bf93113d23e074203c04ccd
Foucsi <julien.foucartreact@gmail.com>
2024-06-22 18:53:11 +0200

    add logout settings screen

--- functions/firstConnection.ts
+++ functions/firstConnection.ts
@@ -1,5 +1,11 @@
 import {AsyncStorage} from 'react-native';
 
-export const firstConnection = () => {
-    const firstConnection = localStorage.setItem('firstConnection', 'true')
+export const firstConnection =async () => {
+    const firstConnection = await AsyncStorage.getItem('firstConnection');
+    if (firstConnection === null) {
+        await AsyncStorage.setItem('firstConnection', 'true');
+        return true;
+    } else {
+        return false;
+    }
 }
