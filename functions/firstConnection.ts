29cb4afd9ad38ed6e6d5a1d347b6226742a69c3d
Foucsi <julien.foucartreact@gmail.com>
2024-06-22 19:23:49 +0200

    add first connection

--- functions/firstConnection.ts
+++ functions/firstConnection.ts
@@ -2,11 +2,16 @@
 import {AsyncStorage} from '@react-native-async-storage/async-storage';
 
 export const firstConnection =async () => {
-    const firstConnection = await AsyncStorage.getItem('firstConnection');
-    if (firstConnection === null) {
-        await AsyncStorage.setItem('firstConnection', 'true');
-        return {success: true,}
-    } else {
-        return {success: false,}
+    try {
+        const firstConnection = await AsyncStorage.getItem("firstConnection");
+        if (firstConnection === null) {
+            await AsyncStorage.setItem("firstConnection", 'true');
+            return { success: true };
+        } else {
+            return { success: false };
+        }
+    } catch (error) {
+        console.error("Error accessing AsyncStorage: ", error);
+        return { success: false, error: 'AsyncStorage error' };
     }
 }
