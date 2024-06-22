66c4fafa2dde51343b56d53338f00e84f8a640d2
Foucsi <julien.foucartreact@gmail.com>
2024-06-22 19:00:12 +0200

    add first connection

---
+++ hooks/getFisrtConnection.ts
@@ -0,0 +1,17 @@
+import {useEffect, useState} from "react";
+import {firstConnection} from "@/functions/firstConnection";
+
+export const getFirstConnection =  () => {
+    const [firstLogin, setFirstLogin] = useState(false)
+    useEffect(()=>{
+        const getIsFirstConnection = async()=>{
+            const result = await firstConnection()
+            if(result.success){
+                setFirstLogin(true)
+            }else{
+                setFirstLogin(false)
+            }
+        }
+    }, [])
+    return {firstLogin}
+}
