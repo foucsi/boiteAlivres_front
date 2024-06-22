ea05c3a259abc3e94dead3dc2dde7ab82a62332b
Foucsi <julien.foucartreact@gmail.com>
2024-06-22 19:06:44 +0200

    add first connection

--- app/(tabs)/mapScreen.tsx
+++ app/(tabs)/mapScreen.tsx
@@ -1,7 +1,6 @@
 import {View, StyleSheet, Text} from 'react-native'
 import React, {useState} from 'react'
 import MapViewComponent from "@/components/MapViewComponent";
-import ModalBookPlaces from "@/components/ModalBookPlaces";
 
 export default function MapScreen() {
     const [modalVisible, setModalVisible] = useState(false)
