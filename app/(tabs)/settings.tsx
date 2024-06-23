d1d6083fb64870d4deb6740ad3359430d8a091f4
Foucsi <julien.foucartreact@gmail.com>
2024-06-23 18:43:56 +0200

    update refacto

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -23,7 +23,7 @@ export default function Settings() {
                 <Text style={styles.mainTitle}>PARAMÈTRES</Text>
                 <Image source={{uri: users?.photo}} style={{width: 100, height: 100}}/>
                 <Text style={styles.text}>Utilisateur: {users?.username}</Text>
-                <Text style={styles.text}>Email: {users.email}</Text>
+                <Text style={styles.text}>Email: {users?.email}</Text>
                 <Text style={styles.text}>Premium: <Entypo name={users.premium ? "check" : "cross"} size={24} color="green" /></Text>
             </View>
             <View style={styles.containerLogout}>
