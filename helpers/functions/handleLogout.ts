d03b53179417269e4fbe717ce9d1e5d4647f72ac
Foucsi <julien.foucartreact@gmail.com>
2024-06-24 15:32:10 +0200

    add useMemo

--- helpers/functions/handleLogout.ts
+++ helpers/functions/handleLogout.ts
@@ -1,8 +1,10 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import {logout} from "@/redux/users";
+import {removeBookSpace} from "@/redux/bookSpaces";
 
 export const handleLogout = async (dispatch:any, router:any, path:any)=>{
     dispatch(logout())
+    dispatch(removeBookSpace())
     await AsyncStorage.removeItem('token')
     router.navigate(path)
 }
