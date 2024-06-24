8a0c23613362a1d790f04db8419af16458ce4d13
Foucsi <julien.foucartreact@gmail.com>
2024-06-24 09:29:14 +0200

    add function send notification

--- components/ModalFirstConnection.tsx
+++ components/ModalFirstConnection.tsx
@@ -14,7 +14,7 @@ export default function ModalFirstConnection({setFirstLogin}) {
                        Pour ajouter une boîte à livres :
 
                        1. Localisation : Effectuez une longue pression sur l'endroit de la carte où se trouve la boîte à livres.
-                       2. Validation : Votre ajout sera soumis à un modérateur pour validation.
+                       2. Validation : Votre ajout sera soumis à un modérateur pour validation sous 24h.
                        3. Affichage : Une fois validée, la boîte à livres sera visible sur la carte avec une icône.
                        4. Personnalisation : Vous pourrez alors ajouter une photo et personnaliser la description du lieu.
                        Bon partage et bonne lecture !</Text>
