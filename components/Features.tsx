60f349f78128290d897042faf74788594de51716
Foucsi <julien.foucartreact@gmail.com>
2024-06-24 16:52:21 +0200

    getAllFeatures

--- components/Features.tsx
+++ components/Features.tsx
@@ -12,11 +12,20 @@ interface FeaturesUser{
 
 export default function Features({feat}:FeaturesUser) {
     const {feature, description, status} = feat
+    const colorStatus ={
+        "En conception": "#294C60",
+        "En développement": "#8FC0A9",
+        "En test": "#F4B942",
+        "Disponible":"#0090C1"
+    }
     return (
-        <View style={styles.containerFeatures}>
+        // @ts-ignore
+        <View style={{backgroundColor:`${colorStatus[status]}`, marginTop:10, padding:10, borderRadius:5}}>
             <Text style={styles.text}>Fonctionnalitée: {feature}</Text>
             <Text style={styles.text}>Description : {description}</Text>
-            <Text style={styles.text}>Status : {status}</Text>
+            <Text style={styles.text}>
+                <Text style={styles.textStatus}>Status : {status}</Text>
+            </Text>
         </View>
     )
 }
@@ -32,5 +41,11 @@ const styles = StyleSheet.create({
     },
     text:{
         color: 'white',
+        fontSize: 16,
+    },
+    textStatus:{
+        color: 'white',
+        fontWeight: 'bold',
+        fontSize: 16,
     }
 })
