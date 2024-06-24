53d8504159dc0c73d50b45463da96350e23cef47
Foucsi <julien.foucartreact@gmail.com>
2024-06-24 16:21:08 +0200

    getAllFeatures

--- app/(tabs)/notification.tsx
+++ app/(tabs)/notification.tsx
@@ -1,14 +1,30 @@
 import {View, Text, StyleSheet, ScrollView} from 'react-native'
 import React from 'react'
+import {useGetAllFeatures} from "@/helpers/hooks/useGetAllFeatures";
+
+interface Features{
+    _id: string,
+    feature: string
+
+}
 
 
 export default function Notification() {
+
+    const {features} = useGetAllFeatures()
+    const allFeatures= features.map((feat:Features)=>{
+        return <View key={feat._id}>
+                <Text>{feat.feature}</Text>
+            </View>
+    })
 
     return (
         <View style={styles.container}>
             <ScrollView>
                 <Text style={styles.mainTitle}>Fonctionnalités à venir</Text>
-                <View style={styles.containerFeatures}></View>
+                <View style={styles.containerFeatures}>
+                    {allFeatures}
+                </View>
             </ScrollView>
 
         </View>
---
+++ helpers/hooks/useGetAllFeatures.ts
@@ -0,0 +1,20 @@
+import {useEffect, useState} from "react";
+import {getAllFeatures} from "@/helpers/functions/getAllFeatures";
+
+export const useGetAllFeatures = () => {
+    const [features, setFeatures] = useState([]);
+
+    useEffect(()=>{
+        const fetchFeatures = async()=>{
+            const result = await getAllFeatures()
+            if(result.success){
+                setFeatures(result.data)
+            }else {
+                console.log(result.error)
+            }
+        }
+         fetchFeatures()
+    }, [features])
+
+    return {features}
+}
