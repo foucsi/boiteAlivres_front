6e73a566372c001a1ebb16603bb03436735b418b
Foucsi <julien.foucartreact@gmail.com>
2024-06-24 15:52:46 +0200

    add useMemo

--- app/(tabs)/notification.tsx
+++ app/(tabs)/notification.tsx
@@ -1,21 +1,13 @@
 import {View, Text, StyleSheet} from 'react-native'
 import React from 'react'
-import { FontAwesome } from '@expo/vector-icons';
 
-import {INFOS_TEXT} from "@/constants/infoText";
 
 export default function Notification() {
 
-    const infosText = INFOS_TEXT('infos')
     return (
         <View style={styles.container}>
-            <View style={styles.containerCenter}>
-                <FontAwesome name="info-circle" size={44} color="#294C60" />
-                <Text style={styles.text}>
-                    {infosText}
-                </Text>
-                <Text>Vous pourrez retrouver sur cette écran toutes les futurs fonctionnalitées en développement</Text>
-            </View>
+            <Text style={styles.mainTitle}>Fonctionnalités à venir</Text>
+
         </View>
     )
 }
@@ -23,16 +15,11 @@ export default function Notification() {
 const styles = StyleSheet.create({
     container:{
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
     },
-    containerCenter:{
-        width: '70%',
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    text:{
+    mainTitle:{
+        marginTop: 90,
         fontSize: 20,
-        textAlign: 'center'
+        marginLeft: 20,
+        fontWeight: 'bold',
     }
 })
