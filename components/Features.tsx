8fd645d04f1a52a1648d8952c782e19eefd5ebfc
Foucsi <julien.foucartreact@gmail.com>
2024-06-24 16:35:48 +0200

    getAllFeatures

--- components/Features.tsx
+++ components/Features.tsx
@@ -1,20 +1,36 @@
-import {View, Text} from 'react-native'
+import {View, Text, StyleSheet} from 'react-native'
 import React from 'react'
 
 interface FeaturesUser{
     _id: string,
     feature: string,
     feat: any,
-    description: string
+    description: string,
+    status: string
 
 }
 
 export default function Features({feat}:FeaturesUser) {
-    const {feature, description} = feat
+    const {feature, description, status} = feat
     return (
-        <View>
-            <Text>Fonctionnalitée: {feature}</Text>
-            <Text>Description : {description}</Text>
+        <View style={styles.containerFeatures}>
+            <Text style={styles.text}>Fonctionnalitée: {feature}</Text>
+            <Text style={styles.text}>Description : {description}</Text>
+            <Text style={styles.text}>Status : {status}</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    containerFeatures:{
+        width:"100%",
+        padding: 20,
+        borderStyle: 'solid',
+        backgroundColor:"#294C60",
+        borderRadius: 10,
+        marginTop: 10,
+    },
+    text:{
+        color: 'white',
+    }
+})
