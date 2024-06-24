3efc44db86c8074d2d30e31fc1de60acb9eaadc4
Foucsi <julien.foucartreact@gmail.com>
2024-06-24 09:28:01 +0200

    add function send notification

--- components/ModalFirstConnection.tsx
+++ components/ModalFirstConnection.tsx
@@ -1,13 +1,23 @@
 import {View, Text, Modal, StyleSheet, TouchableOpacity} from 'react-native'
 import React from 'react'
+import {useSelector} from "react-redux";
 
 // @ts-ignore
 export default function ModalFirstConnection({setFirstLogin}) {
+    const users = useSelector((state: any) => state.user.value);
     return (
         <Modal transparent={true} animationType="slide">
            <View style={styles.modal}>
                <View style={styles.firstConnection}>
-                   <Text>modal first connection</Text>
+                   <Text>Bienvenue dans l'application de partage de boîte à livres !
+
+                       Pour ajouter une boîte à livres :
+
+                       1. Localisation : Effectuez une longue pression sur l'endroit de la carte où se trouve la boîte à livres.
+                       2. Validation : Votre ajout sera soumis à un modérateur pour validation.
+                       3. Affichage : Une fois validée, la boîte à livres sera visible sur la carte avec une icône.
+                       4. Personnalisation : Vous pourrez alors ajouter une photo et personnaliser la description du lieu.
+                       Bon partage et bonne lecture !</Text>
                    <TouchableOpacity onPress={()=>setFirstLogin(false)}>
                        <Text>Close Modal</Text>
                    </TouchableOpacity>
