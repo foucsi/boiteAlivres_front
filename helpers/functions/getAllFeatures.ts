5b6faf88fb372468ed37a07f0765a0853352999b
Foucsi <julien.foucartreact@gmail.com>
2024-06-25 08:41:50 +0200

    add logger

--- helpers/functions/getAllFeatures.ts
+++ helpers/functions/getAllFeatures.ts
@@ -2,14 +2,17 @@ import {URL_GET_ALL_FEATURES} from "@/constants/Url";
 
 export const getAllFeatures = async() => {
     try{
+        //logger.info("getAllFeatures")
         const response = await fetch(URL_GET_ALL_FEATURES)
         if(!response.ok){
             throw new Error(`HTTP error! status: ${response.status} ${response.statusText}`);
         }
         const data = await response.json();
         if(data.result){
+            //logger.info("getAllFeatures", data.features)
             return {success: true, data: data.features}
         }else {
+            //logger.error("getAllFeatures", data.error)
             return {success: false, error: data.error}
         }
     }catch(err){
