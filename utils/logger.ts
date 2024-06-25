1ef49effa9d147716b4126e4a95e56f7196d2675
Foucsi <julien.foucartreact@gmail.com>
2024-06-25 09:10:25 +0200

    add log react native log

--- utils/logger.ts
+++ utils/logger.ts
@@ -1,4 +1,27 @@
 import { logger, consoleTransport } from "react-native-logs";
 
-export const log = logger.createLogger()
+const defaultConfig = {
+    levels: {
+        debug: 0,
+        info: 1,
+        warn: 2,
+        error: 3,
+    },
+    severity: "debug",
+    transport: consoleTransport,
+    transportOptions: {
+        colors: {
+            info: "blueBright",
+            warn: "yellowBright",
+            error: "redBright",
+        },
+    },
+    async: true,
+    dateFormat: "time",
+    printLevel: true,
+    printDate: true,
+    fixedExtLvlLength: false,
+    enabled: true,
+};
 
+export const log = logger.createLogger(defaultConfig);
