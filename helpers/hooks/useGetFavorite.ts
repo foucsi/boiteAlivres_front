37c78eb62421efb62e886864b7fbac0e2a8e59b9
Foucsi <julien.foucartreact@gmail.com>
2024-06-25 18:34:36 +0200

    add model schema favorites

--- helpers/hooks/useGetFavorite.ts
+++ helpers/hooks/useGetFavorite.ts
@@ -5,6 +5,9 @@ export const useGetFavorite = (uniqueId:string, bookPlaceId:string) => {
     const [isFavorite, setIsFavorite] = useState<boolean>(false)
 
     const fetchIsFavorite = useCallback(async() => {
+        if(!bookPlaceId || bookPlaceId === null){
+            return
+        }
         const response = await getFavorite(uniqueId, bookPlaceId)
         if(response.success){
             setIsFavorite(true)
