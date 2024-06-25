a1f836f97a480181f7186f90fed6f0c1511495d6
Foucsi <julien.foucartreact@gmail.com>
2024-06-25 18:09:43 +0200

    add model schema favorites

--- helpers/functions/getFavorite.ts
+++ helpers/functions/getFavorite.ts
@@ -3,8 +3,13 @@ import {log} from "@/utils/logger"
 export const getFavorite = async(uniqueId:string, bookPlaceId:string) => {
     const url = `http://localhost:3000/favorites/allFavoritesByUser/${uniqueId}/${bookPlaceId}`
     try{
-        log.info(`getFavorite: Fetching data from ${url}`)
-        const response = await fetch(url)
+        log.info(`getFavorite: Fetching data for user ${uniqueId} and bookPlace ${bookPlaceId}`);
+        const response = await fetch(url, {
+            method:"GET",
+            headers: {
+                "Accept":"application/json"
+            }
+        })
         if(!response.ok){
             throw new Error(`HTTP error! status: ${response.status} ${response.statusText}`)
         }
