3f009f0e61fa3a5fe56d62ffefc5ad267c6b96e9
Foucsi <julien.foucartreact@gmail.com>
2024-06-25 08:33:25 +0200

    add isLoading useGetAllFeature

--- helpers/hooks/useGetAllFeatures.ts
+++ helpers/hooks/useGetAllFeatures.ts
@@ -4,18 +4,22 @@ import {getAllFeatures} from "@/helpers/functions/getAllFeatures";
 export const useGetAllFeatures = () => {
     const [features, setFeatures] = useState([]);
     const [isLoading, setIsLoading] = useState(false)
+    const [error, setError] = useState(null)
 
     useEffect(()=>{
         const fetchFeatures = async()=>{
+            setIsLoading(true)
             const result = await getAllFeatures()
             if(result.success){
                 setFeatures(result.data)
+                setIsLoading(false)
             }else {
                 console.log(result.error)
+                setError(result.error)
             }
         }
          fetchFeatures()
     }, [])
 
-    return {features}
+    return {features, isLoading, error}
 }
