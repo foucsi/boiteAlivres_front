e530d16ff202e3bedea2e85a7e91a44da55b2514
Foucsi <julien.foucartreact@gmail.com>
2024-06-26 19:09:18 +0200

    add function getFvaoritesUser

--- helpers/hooks/useGetFavoritesUser.ts
+++ helpers/hooks/useGetFavoritesUser.ts
@@ -1,20 +1,23 @@
 import {useEffect, useState} from "react";
 import {getFavoritesUser} from "@/helpers/functions/getFavoritesUser";
 
-interface favoritesUser{
+type Favorite = any
+
+interface FavoritesUser{
     success: boolean,
     data?: any,
     error?: string
 }
 
 export const useGetFavoritesUser = (uniqueId:string) => {
-    const [favorites, setFavorites] = useState([])
+    const [favorites, setFavorites] = useState<Favorite[]>([])
     const [loading, setLoading] = useState(false)
-    const [error, setError] = useState(null)
+    const [error, setError] = useState<string | null>(null)
 
     const getFavorites = async()=>{
         setLoading(true)
-        const result = await getFavoritesUser(uniqueId)
+        // @ts-ignore
+        const result :FavoritesUser = await getFavoritesUser(uniqueId)
         if(result.success){
             setFavorites(result.data)
             setLoading(false)
