ec5a2fbfd289f7752a38623e984b254d807a4acb
Foucsi <julien.foucartreact@gmail.com>
2024-06-26 19:14:28 +0200

    add function getFvaoritesUser

--- components/Favorites.tsx
+++ components/Favorites.tsx
@@ -1,7 +1,18 @@
 import {View, Text} from 'react-native'
 import React from 'react'
+import {useGetFavoritesUser} from "@/helpers/hooks/useGetFavoritesUser";
+import {useSelector} from "react-redux";
 
 export default function Favorites() {
+
+    const uniqueId = useSelector((state: any) => state.user.value.uniqueId)
+
+    const {favorites, loading, error} = useGetFavoritesUser("uniqueId")
+
+    if(loading) return <Text>Loading...</Text>
+    if(error) return <Text>Erreur: {error}</Text>
+
+
     return (
         <View>
             <Text>Favorites</Text>
