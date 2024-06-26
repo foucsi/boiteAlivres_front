2dbaa53aa4174df82a55dda5fb9c92832ddcee1a
Foucsi <julien.foucartreact@gmail.com>
2024-06-26 10:22:12 +0200

    add reducer favorites

---
+++ redux/favoritesSlice.ts
@@ -0,0 +1,22 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+const initialState = {
+    value: [],
+}
+
+export const favoritesSlice = createSlice({
+    name: "favorites",
+    initialState,
+    reducers: {
+        addFavorite: (state, action) => {
+            // @ts-ignore
+            state.value.push(action.payload)
+        },
+        removeFavorite: (state, action) => {
+            state.value = state.value.filter((item) => item !== action.payload)
+        }
+    }
+})
+
+export const {addFavorite, removeFavorite} = favoritesSlice.actions
+export default favoritesSlice.reducer
