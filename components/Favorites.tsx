9af9b9377edc0036eda11ffbf371cff631f57787
Foucsi <julien.foucartreact@gmail.com>
2024-06-27 08:50:16 +0200

    update Favorites.tsx component

--- components/Favorites.tsx
+++ components/Favorites.tsx
@@ -7,7 +7,7 @@ export default function Favorites() {
 
     const uniqueId = useSelector((state: any) => state.user.value.uniqueId)
 
-    const {favorites, loading, error} = useGetFavoritesUser("uniqueId")
+    const {favorites, loading, error} = useGetFavoritesUser(uniqueId)
 
     if(loading) return <Text>Loading...</Text>
     if(error) return <Text>Erreur: {error}</Text>
