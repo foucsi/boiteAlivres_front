6ce56bad74321cbef9f21bdcee73f1c7d193f1e5
Foucsi <julien.foucartreact@gmail.com>
2024-06-28 10:41:02 +0200

    add rte delete favorite

--- helpers/functions/deleteFavorite.ts
+++ helpers/functions/deleteFavorite.ts
@@ -5,7 +5,11 @@ export const deleteFavorite = async(favoriteId:string)=>{
     log.info(`Fetch route ${URL_DELETE_FAVORITE} width favoriteId : ${favoriteId}`)
     const url = ""
         try{
-
+            const response = await fetch(URL_DELETE_FAVORITE(favoriteId),{
+                method: "DELETE",
+                headers: {
+                    "Content-Type": "application/json",}
+            })
         }catch(err){
             console.log(err)
     }
