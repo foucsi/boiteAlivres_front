412d44e28a3ea9132323f2dcd4ea2f93848882bc
Foucsi <julien.foucartreact@gmail.com>
2024-06-30 17:48:26 +0200

    refacto error

--- components/ModalAddMarker.tsx
+++ components/ModalAddMarker.tsx
@@ -16,15 +16,16 @@ export default function ModalAddMarker({modalVisible, setModalVisible}) {
             }}>
             <View style={styles.centeredView}>
                 <View style={styles.modalView}>
-                    <Text style={styles.modalText}>Merci Beaucoup {user.username} !
-                        Bravo ! Votre boîte à livres est en route 📚🎉
-                        🔶 Statut : En attente du feu vert (24h max)
-                        En attendant, pourquoi ne pas lui donner un peu de style ?
-                        👉 Cliquez sur le marqueur de votre boîte et...
-                        ✍️ Racontez son histoire
-                        📸 Montrez son plus beau profil
+                    <Text style={styles.modalText}>
+                        Merci Beaucoup {user.username} !{'\n\n'}
+                        Bravo ! Votre boîte à livres est en route 📚🎉{'\n\n'}
+                        🔶 Statut : En attente du feu vert (24h max){'\n\n'}
+                        En attendant, pourquoi ne pas lui donner un peu de style ?{'\n'}
+                        👉 Cliquez sur le marqueur de votre boîte et...{'\n'}
+                        ✍️ Racontez son histoire{'\n'}
+                        📸 Montrez son plus beau profil{'\n\n'}
                         Personnalisez, amusez-vous, et merci de faire grandir notre communauté de lecteurs !
-                        </Text>
+                    </Text>
                     <Pressable
                         style={[styles.button, styles.buttonClose]}
                         onPress={() => setModalVisible(!modalVisible)}>
