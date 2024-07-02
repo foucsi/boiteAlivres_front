a7c042e1fc9fb11fb6f5e79c0c4adc07ed3f890c
Foucsi <julien.foucartreact@gmail.com>
2024-07-02 15:08:49 +0200

    refacto style

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -36,6 +36,9 @@ export default function RootLayout() {
             <PersistGate persistor={persistor}>
                 <Stack>
                     <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+                    <Stack.Screen name="login" options={{ headerShown: false }} />
+                    <Stack.Screen name="register" options={{ headerShown: false }} />
+                    <Stack.Screen name="index" options={{ headerShown: false }} />
                 </Stack>
             </PersistGate>
         </Provider>
