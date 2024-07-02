9a94442cd4e90eca6d6eb81fb329f08f041f7ad3
Foucsi <julien.foucartreact@gmail.com>
2024-07-02 14:26:47 +0200

    refacto style

--- components/MarkerBookPlaces.tsx
+++ components/MarkerBookPlaces.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Marker} from "react-native-maps";
 import { FontAwesome } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import {View, StyleSheet} from "react-native";
 
 // @ts-ignore
@@ -8,7 +9,7 @@ export default function MarkerBookPlaces({lat,long, description,img, setModalVis
     return (
         <Marker description="Boite à livres" coordinate={{latitude:lat, longitude:long}} >
             <View style={styles(status).containerIcon}>
-                <FontAwesome name={img} size={24} color={status === "pending" ? "orange" : "#294C60"}  onPress={()=>{
+                <MaterialCommunityIcons name={img} size={24} color={status === "pending" ? "orange" : "#294C60"}  onPress={()=>{
                     setModalVisible(true)
                     setSelectedMarker({lat:lat, long:long, photo:photo, description:description , date:date, username:username, photoUser:photoUser, uniqueId:uniqueId, id:id, status:status})
                 }}/>
