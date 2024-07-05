89c98944d42d52fb596bf5215b4339e96041001b
Foucsi <julien.foucartreact@gmail.com>
2024-07-05 07:32:22 +0200

    delete bookPlace

--- helpers/functions/deleteBookPlace.ts
+++ helpers/functions/deleteBookPlace.ts
@@ -10,11 +10,14 @@ export const deleteBookPlace = async (id: string) => {
                 'Content-Type': 'application/json',
             }
         })
+        if(!response.ok){
+            throw new Error(`HTTP error! status: ${response.status}`)
+        }
         const data = await response.json()
         if(data.result){
             log.info(`Deleted place with id success: ${id}`)
         }else {
-            log.error(`Error while deleting place with id: ${id}`)
+            log.warn(`Error while deleting place with id: ${id}`)
         }
     }catch(err){
         log.error('Error while')
