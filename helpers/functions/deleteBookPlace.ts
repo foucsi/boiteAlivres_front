14b47fee6bd175d844200a6a99855e9585c2373d
Foucsi <julien.foucartreact@gmail.com>
2024-07-05 07:28:35 +0200

    delete bookPlace

--- helpers/functions/deleteBookPlace.ts
+++ helpers/functions/deleteBookPlace.ts
@@ -2,7 +2,12 @@ import {URL_DELETE_BOOK_PLACE} from "@/constants/Url";
 
 export const deleteBookPlace = async (id: string) => {
     try{
-
+        const response = await fetch(URL_DELETE_BOOK_PLACE(id), {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        })
     }catch(err){
         console.log(err)
     }
