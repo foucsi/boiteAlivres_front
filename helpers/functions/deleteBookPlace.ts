cda55d23695bc2abc5a861996661563fa5770238
Foucsi <julien.foucartreact@gmail.com>
2024-07-05 07:26:41 +0200

    delete bookPlace

--- helpers/functions/deleteBookPlace.ts
+++ helpers/functions/deleteBookPlace.ts
@@ -1,1 +1,9 @@
-export const deleteBookPlace = async (id: string) => {}
+import {URL_DELETE_BOOK_PLACE} from "@/constants/Url";
+
+export const deleteBookPlace = async (id: string) => {
+    try{
+
+    }catch(err){
+        console.log(err)
+    }
+}
