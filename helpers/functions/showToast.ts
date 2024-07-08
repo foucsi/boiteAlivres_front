96fc8009cb8fc677a0483e353e1a8e20c3f67985
Foucsi <julien.foucartreact@gmail.com>
2024-07-08 23:14:48 +0200

    update toast

--- helpers/functions/showToast.ts
+++ helpers/functions/showToast.ts
@@ -1,11 +1,11 @@
 import Toast from 'react-native-toast-message'
 
-export const showModal = (text1:string, text2:string)=>{
+export const showModal = (text1:string, text2:string, position:string)=>{
     Toast.show({
         type: 'success',
         text1: text1,
         text2: text2,
-        position: 'bottom',
+        position: position,
         visibilityTime: 2000,
         autoHide: true,
         bottomOffset: 50,
