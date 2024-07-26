ba796d7eb561716b64a4724401d06f4cf0c125f4
Foucsi <julien.foucartreact@gmail.com>
2024-07-26 11:00:16 +0200

    update add function getAllBookPlaceByUser

---
+++ helpers/hooks/useGetAllBookPlaceByUserId.ts
@@ -0,0 +1,21 @@
+import {useEffect, useState} from "react";
+import {getAllBookPlaceByUser} from "@/helpers/functions/getAllBookPlaceByUser";
+
+export const useGetAllBookPlaceByUserId = (uniqueId: string) => {
+    const [bookPlacesLength, setBookPlacesLength] = useState<number>(0)
+
+    const fetchAllBookPlacesByUserId = async()=>{
+        const result = await getAllBookPlaceByUser(uniqueId)
+        if(result.success) {
+            setBookPlacesLength(result.data.length)
+        }else {
+            console.log(result.error)
+        }
+    }
+
+    useEffect(() => {
+        fetchAllBookPlacesByUserId()
+    }, [uniqueId]);
+
+    return {bookPlacesLength}
+}
