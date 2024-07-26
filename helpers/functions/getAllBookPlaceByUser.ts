1e8f051ad057f4647745c4a803c380bc5a37cdf0
Foucsi <julien.foucartreact@gmail.com>
2024-07-26 10:50:01 +0200

    update add function getAllBookPlaceByUser

--- helpers/functions/getAllBookPlaceByUser.ts
+++ helpers/functions/getAllBookPlaceByUser.ts
@@ -4,7 +4,11 @@ import {URL_GET_BOOKPLACE_BY_USER_ID} from "@/constants/Url";
 export const getAllBookPlaceByUser = async (uniqueId: string) => {
     try{
         const response = await fetch(URL_GET_BOOKPLACE_BY_USER_ID(uniqueId))
+        if(!response.ok){
+            throw new Error(`HTTP error! status: ${response.status} ${response.statusText}`)
+        }
         const data = await response.json()
+
 
         if(data.result){
             return {success: true, data: data.bookPlaces}
