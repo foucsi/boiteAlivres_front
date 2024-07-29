f38d83a5081a1b289fbee73156bb1f483f418baa
Foucsi <julien.foucartreact@gmail.com>
2024-07-29 20:28:05 +0200

    test react query

--- helpers/hooks/useGetAllBookPlaces.ts
+++ helpers/hooks/useGetAllBookPlaces.ts
@@ -4,30 +4,20 @@ import {getAllBookPlaces} from "@/helpers/functions/getAllBookPlaces";
 import {useSelector} from "react-redux";
 
 export const useGetAllBookPlaces = () => {
-    // const [bookSpaces, setBookSpaces] = useState([]);
+    const [bookSpaces, setBookSpaces] = useState([]);
     const bookPlace = useSelector((state: any) => state.bookSpace.value)
-    // useEffect(()=>{
-    //     const fetchBookPlaces = async()=>{
-    //         const result = await getAllBookPlaces()
-    //         if(result.success){
-    //             setBookSpaces(result.bookPlaces)
-    //         }else {
-    //             console.log(result.error)
-    //         }
-    //     }
-    //    fetchBookPlaces()
-    // }, [bookPlace.bookSpaces, bookPlace.description, bookPlace.photo])
-    //
-    // return {bookSpaces}
-
-    return useQuery(
-        ['bookPlaces', bookPlace.bookSpaces, bookPlace.description, bookPlace.photo],
-        getAllBookPlaces,
-        {
-            onError: (error: Error) => {
-                console.log(error.message);
-            },
-            refetchOnWindowFocus: false,
+    useEffect(()=>{
+        const fetchBookPlaces = async()=>{
+            const result = await getAllBookPlaces()
+            if(result.success){
+                setBookSpaces(result.bookPlaces)
+            }else {
+                console.log(result.error)
+            }
         }
-    );
+       fetchBookPlaces()
+    }, [bookPlace.bookSpaces, bookPlace.description, bookPlace.photo])
+
+    return {bookSpaces}
+
 }
