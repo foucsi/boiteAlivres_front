8781b0df8e7ee59691c9b93852c540555c01948f
Foucsi <julien.foucartreact@gmail.com>
2024-07-30 10:27:42 +0200

    add react query encapsulation

--- helpers/hooks/useGetAllFeatures.ts
+++ helpers/hooks/useGetAllFeatures.ts
@@ -1,4 +1,5 @@
 import {useEffect, useState} from "react";
+import {useQuery} from "react-query";
 import {getAllFeatures} from "@/helpers/functions/getAllFeatures";
 
 export const useGetAllFeatures = () => {
