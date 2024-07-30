cd17bc0e5a907f25862f11fe602c96dd5641d079
Foucsi <julien.foucartreact@gmail.com>
2024-07-30 10:30:09 +0200

    react query implementation

--- helpers/hooks/useGetAllFeatures.ts
+++ helpers/hooks/useGetAllFeatures.ts
@@ -1,30 +1,8 @@
-import {useEffect, useState} from "react";
 import {useQuery} from "react-query";
 import {getAllFeatures} from "@/helpers/functions/getAllFeatures";
 
 export const useGetAllFeatures = () => {
-    // const [features, setFeatures] = useState([]);
-    // const [isLoading, setIsLoading] = useState(false)
-    // const [error, setError] = useState(null)
-
-    // useEffect(()=>{
-    //     const fetchFeatures = async()=>{
-    //         setIsLoading(true)
-    //         const result = await getAllFeatures()
-    //         if(result.success){
-    //             setFeatures(result.data)
-    //             setIsLoading(false)
-    //         }else {
-    //             console.log(result.error)
-    //             setError(result.error)
-    //         }
-    //     }
-    //      fetchFeatures()
-    // }, [])
-    //
-    // return {features, isLoading, error}
     const {data, isLoading, error} = useQuery('features', getAllFeatures)
-
     return {
         features: data?.data || [],
         isLoading,
