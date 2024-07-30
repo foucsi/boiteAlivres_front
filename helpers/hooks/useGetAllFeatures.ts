ecfce9f731944faced75ef00666d65dfa470095d
Foucsi <julien.foucartreact@gmail.com>
2024-07-30 10:29:24 +0200

    add react query encapsulation

--- helpers/hooks/useGetAllFeatures.ts
+++ helpers/hooks/useGetAllFeatures.ts
@@ -3,24 +3,31 @@ import {useQuery} from "react-query";
 import {getAllFeatures} from "@/helpers/functions/getAllFeatures";
 
 export const useGetAllFeatures = () => {
-    const [features, setFeatures] = useState([]);
-    const [isLoading, setIsLoading] = useState(false)
-    const [error, setError] = useState(null)
+    // const [features, setFeatures] = useState([]);
+    // const [isLoading, setIsLoading] = useState(false)
+    // const [error, setError] = useState(null)
 
-    useEffect(()=>{
-        const fetchFeatures = async()=>{
-            setIsLoading(true)
-            const result = await getAllFeatures()
-            if(result.success){
-                setFeatures(result.data)
-                setIsLoading(false)
-            }else {
-                console.log(result.error)
-                setError(result.error)
-            }
-        }
-         fetchFeatures()
-    }, [])
+    // useEffect(()=>{
+    //     const fetchFeatures = async()=>{
+    //         setIsLoading(true)
+    //         const result = await getAllFeatures()
+    //         if(result.success){
+    //             setFeatures(result.data)
+    //             setIsLoading(false)
+    //         }else {
+    //             console.log(result.error)
+    //             setError(result.error)
+    //         }
+    //     }
+    //      fetchFeatures()
+    // }, [])
+    //
+    // return {features, isLoading, error}
+    const {data, isLoading, error} = useQuery('features', getAllFeatures)
 
-    return {features, isLoading, error}
+    return {
+        features: data?.data || [],
+        isLoading,
+        error
+    }
 }
