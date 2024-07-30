69aa26834e144afd78d61dff0a860dba27413396
Foucsi <julien.foucartreact@gmail.com>
2024-07-30 19:17:37 +0200

    react query implementatiuon in useGetFavoriteUser.ts

--- helpers/hooks/useGetFavoritesUser.ts
+++ helpers/hooks/useGetFavoritesUser.ts
@@ -1,4 +1,4 @@
-import {useEffect, useState} from "react";
+import {useQuery} from "react-query";
 import {getFavoritesUser} from "@/helpers/functions/getFavoritesUser";
 import {useSelector} from "react-redux";
 
@@ -11,30 +11,41 @@ interface FavoritesUser{
 }
 
 export const useGetFavoritesUser = (uniqueId:string) => {
-    const [favorites, setFavorites] = useState<Favorite[]>([])
-    const [loading, setLoading] = useState(false)
-    const [error, setError] = useState<string | null>(null)
+    // const [favorites, setFavorites] = useState<Favorite[]>([])
+    // const [loading, setLoading] = useState(false)
+    // const [error, setError] = useState<string | null>(null)
+    //
+    // // @ts-ignore
+    // const favo = useSelector((state)=> state.favorite.value)
+    //
+    // const getFavorites = async()=>{
+    //     console.log("favo:", favo)
+    //     setLoading(true)
+    //     // @ts-ignore
+    //     const result :FavoritesUser = await getFavoritesUser(uniqueId)
+    //     if(result.success){
+    //         setFavorites(result.data)
+    //         setLoading(false)
+    //     }else {
+    //         setError(result.error || "Une erreur est survenue")
+    //         setLoading(false)
+    //     }
+    // }
+    //
+    // useEffect(() => {
+    //     getFavorites()
+    // }, [favo])
+    //
+    // return {favorites, loading, error}
+    const {data, isLoading, error}=useQuery(['favorites', uniqueId], ()=>getFavoritesUser(uniqueId), {
+        enabled: !!uniqueId
 
-    // @ts-ignore
-    const favo = useSelector((state)=> state.favorite.value)
+    })
 
-    const getFavorites = async()=>{
-        console.log("favo:", favo)
-        setLoading(true)
+    return {
+        favorites: data?.data || [],
+        isLoading,
         // @ts-ignore
-        const result :FavoritesUser = await getFavoritesUser(uniqueId)
-        if(result.success){
-            setFavorites(result.data)
-            setLoading(false)
-        }else {
-            setError(result.error || "Une erreur est survenue")
-            setLoading(false)
-        }
+        error: error?.message || error
     }
-
-    useEffect(() => {
-        getFavorites()
-    }, [favo])
-
-    return {favorites, loading, error}
 }
