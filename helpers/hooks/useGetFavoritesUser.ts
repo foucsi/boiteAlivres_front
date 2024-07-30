4572c7a73b0ba2f4bb654e6030bef1e6412eece4
Foucsi <julien.foucartreact@gmail.com>
2024-07-30 19:19:39 +0200

    react query implementatiuon in useGetFavoriteUser.ts

--- helpers/hooks/useGetFavoritesUser.ts
+++ helpers/hooks/useGetFavoritesUser.ts
@@ -11,35 +11,9 @@ interface FavoritesUser{
 }
 
 export const useGetFavoritesUser = (uniqueId:string) => {
-    // const [favorites, setFavorites] = useState<Favorite[]>([])
-    // const [loading, setLoading] = useState(false)
-    // const [error, setError] = useState<string | null>(null)
-    //
-    // // @ts-ignore
-    // const favo = useSelector((state)=> state.favorite.value)
-    //
-    // const getFavorites = async()=>{
-    //     console.log("favo:", favo)
-    //     setLoading(true)
-    //     // @ts-ignore
-    //     const result :FavoritesUser = await getFavoritesUser(uniqueId)
-    //     if(result.success){
-    //         setFavorites(result.data)
-    //         setLoading(false)
-    //     }else {
-    //         setError(result.error || "Une erreur est survenue")
-    //         setLoading(false)
-    //     }
-    // }
-    //
-    // useEffect(() => {
-    //     getFavorites()
-    // }, [favo])
-    //
-    // return {favorites, loading, error}
     const {data, isLoading, error}=useQuery(['favorites', uniqueId], ()=>getFavoritesUser(uniqueId), {
         enabled: !!uniqueId
-
+        //enable sert a verifier si le param uniqueId est bien present
     })
 
     return {
