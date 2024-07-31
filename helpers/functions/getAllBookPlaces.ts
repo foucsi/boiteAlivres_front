4747289c7539b3e1b28b687a9779db5e8289dd33
Foucsi <julien.foucartreact@gmail.com>
2024-07-31 15:22:26 +0200

    refacto getAllBookPlaces.ts

--- helpers/functions/getAllBookPlaces.ts
+++ helpers/functions/getAllBookPlaces.ts
@@ -2,7 +2,6 @@ import {URL_GET_ALL_BOOK_PLACES} from "@/constants/Url";
 import {log} from "@/utils/logger";
 
 export const getAllBookPlaces = async () => {
-        log.info("getAllBookPlaces", `Fetching all bookPlaces on url ${URL_GET_ALL_BOOK_PLACES}`);
         const response = await fetch(URL_GET_ALL_BOOK_PLACES);
         if(!response.ok){
             throw new Error(`HTTP error! status: ${response.status} ${response.statusText}`);
