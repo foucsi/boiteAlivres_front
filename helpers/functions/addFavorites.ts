f50c7ff84e7d89a156ac1ab325cb884b4207b0c4
Foucsi <julien.foucartreact@gmail.com>
2024-08-02 14:17:31 +0200

    add useMutation addFavorite.ts and add hook with useMutation

--- helpers/functions/addFavorites.ts
+++ helpers/functions/addFavorites.ts
@@ -1,6 +1,6 @@
 import {URL_ADD_FAVORITE} from "@/constants/Url";
 
-export const addFavorites = async (uniqueId:string, bookPlaceId:string)=>{
+export const addFavorites = async ({uniqueId, bookPlaceId} : {uniqueId:string, bookPlaceId:string})=>{
         const response = await fetch(URL_ADD_FAVORITE(uniqueId, bookPlaceId), {
             method: 'POST',
             headers: {
