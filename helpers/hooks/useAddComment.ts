b961f8e2f6d71961d66f18eefd542c50c3aa7e64
Foucsi <julien.foucartreact@gmail.com>
2024-08-02 09:09:17 +0200

    add useMutation addComment.ts

--- helpers/hooks/useAddComment.ts
+++ helpers/hooks/useAddComment.ts
@@ -1,35 +1,31 @@
-import {useState} from "react";
-import {addComment} from "@/helpers/functions/addComment";
-import {useDispatch, useSelector} from "react-redux";
-import {addCommentReducer} from "@/redux/comments";
-import {useMutation} from "react-query";
+import { useState } from "react";
+import { addComment } from "@/helpers/functions/addComment";
+import { useDispatch, useSelector } from "react-redux";
+import { addCommentReducer } from "@/redux/comments";
+import { useMutation } from "react-query";
 
 // @ts-ignore
-export const useAddComment = (bookPlaceId)=>{
-    const [comment, setComment] = useState('')
-    const user = useSelector((state: any) => state.user.value)
-    const dispatch = useDispatch()
-    // @ts-ignore
-    // const newComment = async()=>{
-    //     const result = await addComment(user.uniqueId, bookPlaceId, comment)
-    //     if(result.success){
-    //         setComment('')
-    //         dispatch(addCommentReducer(result.comment))
-    //     }else {
-    //         console.log("error add comment: ", result.error)
-    //     }
-    // }
-    //
-    // return {comment, setComment, newComment}
+export const useAddComment = (bookPlaceId) => {
+    const [comment, setComment] = useState('');
+    const user = useSelector((state: any) => state.user.value);
+    const dispatch = useDispatch();
 
-    const {mutate : newComment} = useMutation(()=> addComment(user.uniqueId,bookPlaceId, comment), {
-        onSuccess:(data)=>{
-            dispatch(addCommentReducer(data.comment))
-            setComment('')
-        },
-        onError:(err)=> {
-            console.log("Error adding comment: ", err)
+    const mutation = useMutation(
+        async () => await addComment({ uniqueId: user.uniqueId, bookPlaceId, comment }),
+        {
+            onSuccess: (data) => {
+                dispatch(addCommentReducer(data.comment));
+                setComment('');
+            },
+            onError: (err) => {
+                console.log("Erreur lors de l'ajout du commentaire : ", err);
+            }
         }
-    })
-    return {comment, setComment,newComment}
-}
+    );
+
+    const newComment = () => {
+        mutation.mutate();
+    };
+
+    return { comment, setComment, newComment };
+};
