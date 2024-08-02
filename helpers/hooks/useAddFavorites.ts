1652791c4bdac14dc1b2799b69e56933978a068d
Foucsi <julien.foucartreact@gmail.com>
2024-08-02 14:26:42 +0200

    add useMutation addFavorite.ts and add hook with useMutation

--- helpers/hooks/useAddFavorites.ts
+++ helpers/hooks/useAddFavorites.ts
@@ -6,19 +6,6 @@ import {useMutation} from "react-query";
 
 export const useAddFavorites = (uniqueId:string, bookPlaceId:string) => {
     const dispatch = useDispatch()
-    const user = useSelector((state: any) => state.user.value)
-
-    // const addFavorite = async() => {
-    //     const result = await addFavorites(uniqueId, bookPlaceId)
-    //     if(result.success){
-    //         console.log(result.success)
-    //         dispatch(addFavoriteReducer(result.favorite))
-    //         showModal('Success', 'Boites à livres ajoutées à vos favoris', 'bottom', 4000)
-    //     }else {
-    //         console.log(result.error)
-    //     }
-    // }
-    // return { addFavorite}
     const {mutate : addFavorite} = useMutation(()=>addFavorites({uniqueId , bookPlaceId}), {
         onError: (error) => {
             console.log(error)
