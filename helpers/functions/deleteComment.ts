fae0bc1c623c6a5e78a4da0a64c80bbb641c42c6
Foucsi <julien.foucartreact@gmail.com>
2024-08-03 23:05:50 +0200

    update typage deleteComment

--- helpers/functions/deleteComment.ts
+++ helpers/functions/deleteComment.ts
@@ -1,7 +1,12 @@
 import {URL_DELETE_COMMENT} from "@/constants/Url";
 import {errorResponse} from "@/constants/errors";
 
-export const deleteComment = async ({ uniqueId, commentId }: { uniqueId: string; commentId: string }) => {
+interface DeleteCommentParams{
+    uniqueId: string;
+    commentId: string;
+}
+
+export const deleteComment = async ({ uniqueId, commentId }: DeleteCommentParams) => {
     const url = URL_DELETE_COMMENT(uniqueId);
     const response = await fetch(url, {
         method: "DELETE",
