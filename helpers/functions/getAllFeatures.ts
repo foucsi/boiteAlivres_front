48172326561e53320f8f1ff47a926c72a418cb92
Foucsi <julien.foucartreact@gmail.com>
2024-08-03 12:37:25 +0200

    refdacton

--- helpers/functions/getAllFeatures.ts
+++ helpers/functions/getAllFeatures.ts
@@ -2,22 +2,10 @@ import {URL_GET_ALL_FEATURES} from "@/constants/Url";
 import {log} from "@/utils/logger";
 
 export const getAllFeatures = async() => {
-    try{
         log.info("fetch getAllFeatures")
         const response = await fetch(URL_GET_ALL_FEATURES)
         if(!response.ok){
             throw new Error(`HTTP error! status: ${response.status} ${response.statusText}`);
         }
-        const data = await response.json();
-        if(data.result){
-            log.info("getAllFeatures succefful", data.features)
-            return {success: true, data: data.features}
-        }else {
-            log.error("getAllFeatures", data.error || "Error")
-            return {success: false, error: data.error}
-        }
-    }catch(err){
-        log.error("getAllFeatures", err)
-        return {success: false, error: err}
-    }
+        return response.json()
 }
