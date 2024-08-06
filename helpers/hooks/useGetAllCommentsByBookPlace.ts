1b0f1ea9a242e16cffbed06bb2436c3b7ed78dc3
Foucsi <julien.foucartreact@gmail.com>
2024-08-06 14:36:37 +0200

    refacto code global

--- helpers/hooks/useGetAllCommentsByBookPlace.ts
+++ helpers/hooks/useGetAllCommentsByBookPlace.ts
@@ -1,31 +1,40 @@
-import { useEffect, useState } from "react";
 import { getAllCommentsByBookPlace } from "@/helpers/functions/getAllCommentsByBookPlace";
-import { useSelector } from "react-redux";
+import {useQuery} from "react-query";
+import {useSelector} from "react-redux";
+import {useEffect} from "react";
 
 export const useGetAllCommentsByBookPlace = (bookPlaceId:string) => {
-    const [comments, setComments] = useState([]);
-    const [error, setError] = useState(null);
-    const [loading, setLoading] = useState(true);
-
+    // const [comments, setComments] = useState([]);
+    // const [error, setError] = useState(null);
+    // const [loading, setLoading] = useState(true);
+    //
     // @ts-ignore
     const commentReducer = useSelector((state) => state.comment.value);
+    //
+    // useEffect(() => {
+    //     const fetchAllComments = async () => {
+    //         setLoading(true);
+    //         if (!bookPlaceId) return;
+    //             const result = await getAllCommentsByBookPlace(bookPlaceId);
+    //             if (result.success) {
+    //                 setComments(result.comments);
+    //                 setLoading(false);
+    //             } else {
+    //                 // @ts-ignore
+    //                 setError("Impossible de charger les commentaires");
+    //             }
+    //     };
+    //
+    //     fetchAllComments();
+    // }, [bookPlaceId, commentReducer.comments]);
+    //
+    // return { comments, error, loading };
 
+    const {data, isLoading, error, refetch} = useQuery("comments", ()=>getAllCommentsByBookPlace(bookPlaceId))
     useEffect(() => {
-        const fetchAllComments = async () => {
-            setLoading(true);
-            if (!bookPlaceId) return;
-                const result = await getAllCommentsByBookPlace(bookPlaceId);
-                if (result.success) {
-                    setComments(result.comments);
-                    setLoading(false);
-                } else {
-                    // @ts-ignore
-                    setError("Impossible de charger les commentaires");
-                }
-        };
+        refetch()
+    }, [commentReducer.comments]);
 
-        fetchAllComments();
-    }, [bookPlaceId, commentReducer.comments]);
 
-    return { comments, error, loading };
+    return {comments: data?.comments || [], error, isLoading};
 };
