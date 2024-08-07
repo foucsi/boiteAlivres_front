82487526cd5283ae5ec63a9f6b61f7b62d4f1111
Foucsi <julien.foucartreact@gmail.com>
2024-08-07 09:17:47 +0200

    refacto code global

--- helpers/hooks/useGetAllBookPlaceByUserId.ts
+++ helpers/hooks/useGetAllBookPlaceByUserId.ts
@@ -1,21 +1,24 @@
-import {useEffect, useState} from "react";
 import {getAllBookPlaceByUser} from "@/helpers/functions/getAllBookPlaceByUser";
+import {useQuery} from "react-query";
 
 export const useGetAllBookPlaceByUserId = (uniqueId: string) => {
-    const [bookPlacesLength, setBookPlacesLength] = useState<number>(0)
+    // const [bookPlacesLength, setBookPlacesLength] = useState<number>(0)
+    //
+    // const fetchAllBookPlacesByUserId = async()=>{
+    //     const result = await getAllBookPlaceByUser(uniqueId)
+    //     if(result.success) {
+    //         setBookPlacesLength(result.data)
+    //     }else {
+    //         console.log(result.error)
+    //     }
+    // }
+    //
+    // useEffect(() => {
+    //     fetchAllBookPlacesByUserId()
+    // }, [uniqueId]);
+    //
+    // return {bookPlacesLength}
+    const {data, isLoading , error} = useQuery(["bookPlaces", uniqueId], () => getAllBookPlaceByUser(uniqueId))
 
-    const fetchAllBookPlacesByUserId = async()=>{
-        const result = await getAllBookPlaceByUser(uniqueId)
-        if(result.success) {
-            setBookPlacesLength(result.data)
-        }else {
-            console.log(result.error)
-        }
-    }
-
-    useEffect(() => {
-        fetchAllBookPlacesByUserId()
-    }, [uniqueId]);
-
-    return {bookPlacesLength}
+    return {bookPlacesLength: data?.bookPlaces.length || null, isLoading, error}
 }
