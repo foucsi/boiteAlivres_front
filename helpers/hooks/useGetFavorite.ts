0384e4cae7e3db3e2287c4feb8d51a85a51a8c6e
Foucsi <julien.foucartreact@gmail.com>
2024-08-07 21:38:21 +0200

    refacto code global

--- helpers/hooks/useGetFavorite.ts
+++ helpers/hooks/useGetFavorite.ts
@@ -1,27 +1,37 @@
-import {useEffect, useState, useCallback} from "react";
 import {getFavorite} from "@/helpers/functions/getFavorite";
 import {useSelector} from "react-redux";
+import {useQuery} from "react-query";
 
 export const useGetFavorite = (uniqueId:string, bookPlaceId:string) => {
-    const [isFavorite, setIsFavorite] = useState<boolean>(false)
-    // @ts-ignore
+    // const [isFavorite, setIsFavorite] = useState<boolean>(false)
+    // // @ts-ignore
     const favo = useSelector((state)=> state.favorite.value)
-    const fetchIsFavorite = useCallback(async() => {
-        if(!bookPlaceId || bookPlaceId === null){
-            return
-        }
-        const response = await getFavorite(uniqueId, bookPlaceId)
-        if(response.success){
-            setIsFavorite(true)
-        }else{
-            setIsFavorite(false)
-        }
-    }, [uniqueId, bookPlaceId])
+    // const fetchIsFavorite = useCallback(async() => {
+    //     if(!bookPlaceId || bookPlaceId === null){
+    //         return
+    //     }
+    //     const response = await getFavorite(uniqueId, bookPlaceId)
+    //     if(response.success){
+    //         setIsFavorite(true)
+    //     }else{
+    //         setIsFavorite(false)
+    //     }
+    // }, [uniqueId, bookPlaceId])
+    //
+    //
+    // useEffect(()=>{
+    //         fetchIsFavorite()
+    // }, [fetchIsFavorite, favo])
+    //
+    // return {isFavorite}
 
+    const {data, isLoading, error} = useQuery(["getFavorite", favo], () => getFavorite({uniqueId, bookPlaceId}), {
+        enabled: !!uniqueId && !!bookPlaceId
+    })
 
-    useEffect(()=>{
-            fetchIsFavorite()
-    }, [fetchIsFavorite, favo])
-
-    return {isFavorite}
+    return {
+        isFavorite : data.result,
+        isLoading,
+        error
+    }
 }
