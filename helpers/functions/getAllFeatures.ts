99e4a3c862da023c775e0c9120c1638c7bd589d4
Foucsi <julien.foucartreact@gmail.com>
2024-08-07 11:00:14 +0200

    refacto code global

--- helpers/functions/getAllFeatures.ts
+++ helpers/functions/getAllFeatures.ts
@@ -1,11 +1,12 @@
 import {URL_GET_ALL_FEATURES} from "@/constants/Url";
 import {log} from "@/utils/logger";
+import {errorResponse} from "@/constants/errors";
 
 export const getAllFeatures = async() => {
         log.info("fetch getAllFeatures")
         const response = await fetch(URL_GET_ALL_FEATURES)
         if(!response.ok){
-            throw new Error(`HTTP error! status: ${response.status} ${response.statusText}`);
+            throw new Error(errorResponse(response.status, response.url));
         }
         return await response.json()
 }
