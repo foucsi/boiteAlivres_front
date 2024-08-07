e20e7ba2101f01bb00eb232dc75f40b47c44583a
Foucsi <julien.foucartreact@gmail.com>
2024-08-07 09:28:57 +0200

    refacto code global

--- helpers/hooks/useGetAllBookPlaceByUserId.ts
+++ helpers/hooks/useGetAllBookPlaceByUserId.ts
@@ -2,6 +2,8 @@ import {getAllBookPlaceByUser} from "@/helpers/functions/getAllBookPlaceByUser";
 import {useQuery} from "react-query";
 
 export const useGetAllBookPlaceByUserId = (uniqueId: string) => {
-    const {data, isLoading , error} = useQuery(["bookPlaces", uniqueId], () => getAllBookPlaceByUser({uniqueId}))
+    const {data, isLoading , error} = useQuery(["bookPlaces", uniqueId], () => getAllBookPlaceByUser({uniqueId}), {
+        enabled: !!uniqueId
+    })
     return {bookPlacesLength: data?.bookPlaces.length || 0, isLoading, error}
 }
