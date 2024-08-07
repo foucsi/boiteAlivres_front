5176acc000282ab4ba08392acc006fd7c0d28673
Foucsi <julien.foucartreact@gmail.com>
2024-08-07 21:53:57 +0200

    refacto code global

--- helpers/hooks/useGetFavorite.ts
+++ helpers/hooks/useGetFavorite.ts
@@ -3,29 +3,9 @@ import {useSelector} from "react-redux";
 import {useQuery} from "react-query";
 
 export const useGetFavorite = (uniqueId:string, bookPlaceId:string) => {
-    // const [isFavorite, setIsFavorite] = useState<boolean>(false)
     // @ts-ignore
     const favo = useSelector((state: RootState) => state.favorite.value)
-    // const fetchIsFavorite = useCallback(async() => {
-    //     if(!bookPlaceId || bookPlaceId === null){
-    //         return
-    //     }
-    //     const response = await getFavorite(uniqueId, bookPlaceId)
-    //     if(response.success){
-    //         setIsFavorite(true)
-    //     }else{
-    //         setIsFavorite(false)
-    //     }
-    // }, [uniqueId, bookPlaceId])
-    //
-    //
-    // useEffect(()=>{
-    //         fetchIsFavorite()
-    // }, [fetchIsFavorite, favo])
-    //
-    // return {isFavorite}
-
-    const {data} = useQuery(["getFavorite", favo], () => getFavorite({uniqueId, bookPlaceId}), {
+    const {data} = useQuery(["getFavorite", favo, uniqueId, bookPlaceId], () => getFavorite({uniqueId, bookPlaceId}), {
         enabled: !!uniqueId && !!bookPlaceId
     })
 
