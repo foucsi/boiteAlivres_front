f63f8fb8a845a1601c5ba40b7f6e7a55ba7b73c9
Foucsi <julien.foucartreact@gmail.com>
2024-08-12 15:12:11 +0200

    create file typage interface favorite

--- helpers/hooks/useDeleteCommentMutation.ts
+++ helpers/hooks/useDeleteCommentMutation.ts
@@ -5,14 +5,16 @@ import {deleteComment} from "@/helpers/functions/deleteComment";
 
 export const useDeleteCommentMutation = (uniqueId:string, commentId:string) => {
     const dispatch = useDispatch();
+
+    const handleError = (error: unknown) => {
+        console.error("Error deleting comment:", error);
+    }
     const { mutate: delComment} = useMutation(()=>deleteComment({uniqueId, commentId}), {
         onSuccess: () => {
             console.log("Comment deleted");
             dispatch(removeCommentReducer());
         },
-        onError: (err) => {
-            console.log("Error deleting comment:", err);
-        }
+        onError: handleError
     });
 
     // @ts-ignore
