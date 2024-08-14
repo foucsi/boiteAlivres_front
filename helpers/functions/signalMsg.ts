84af157ed2d96ba5514c57099aa95f8605285b40
Foucsi <julien.foucartreact@gmail.com>
2024-08-14 10:53:48 +0200

    add fucntion signalMsg.ts

--- helpers/functions/signalMsg.ts
+++ helpers/functions/signalMsg.ts
@@ -1,3 +1,16 @@
 import {URL_SEND_SIGNAL} from "@/constants/Url";
 
-export const signalMsg = (msg: string, email:string) => {}
+export const signalMsg = async(msg: string, email:string) => {
+    const response = await fetch(URL_SEND_SIGNAL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({msg, email})
+    })
+
+    if(!response.ok){
+        throw new Error('Error while sending signal message')
+    }
+    return await response.json()
+}
