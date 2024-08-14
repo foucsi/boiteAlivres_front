07d5c4db044259b2e6574a0659d28b47008b8884
Foucsi <julien.foucartreact@gmail.com>
2024-08-14 14:27:50 +0200

    custom modal send signal msg

--- components/ModalSendSignal.tsx
+++ components/ModalSendSignal.tsx
@@ -1,7 +1,9 @@
-import {View, Text, Modal, StyleSheet, TouchableOpacity} from 'react-native'
+import {View, Text, Modal, StyleSheet, TouchableOpacity, TextInput} from 'react-native'
 import React, {useState} from 'react'
 import {useSignalMsg} from "@/helpers/hooks/useSignalMsg";
 import {useSelector} from "react-redux";
+
+import { AntDesign } from '@expo/vector-icons';
 
 // @ts-ignore
 export default function ModalSendSignal({setShowModal, selectedMarker}) {
@@ -9,14 +11,19 @@ export default function ModalSendSignal({setShowModal, selectedMarker}) {
     const user = useSelector((state:any) => state.user.value)
     const {sendSignalMsg, error} = useSignalMsg(message, user.email)
 
+    const closeModal = ()=>{
+        setShowModal((prev: any) => !prev)
+    }
+
     return (
         <Modal animationType="slide" transparent={true}>
             <View style={styles.modal}>
-                <View style={styles.containerCenter}>
-                    <Text>Test signal message</Text>
-                    <TouchableOpacity onPress={()=>setShowModal(false)}>
-                        <Text>close</Text>
-                    </TouchableOpacity>
+                <View style={styles.modalContainer}>
+                    <View style={{padding:5}}>
+                        <TouchableOpacity>
+                            <AntDesign name="closecircleo" size={24} color="#294C60" onPress={closeModal}/>
+                        </TouchableOpacity>
+                    </View>
                     {error ? <Text>Failed to send signal message. Please try again.</Text>: null}
                 </View>
             </View>
@@ -31,7 +38,19 @@ const styles = StyleSheet.create({
         justifyContent:"center",
         backgroundColor: 'rgba(0,0,0,0.5)',
     },
-    containerCenter:{
-        backgroundColor: 'white',
-    }
+    modalContainer:{
+        padding: 10,
+        height:200,
+        width:300,
+        backgroundColor:"white",
+        borderRadius: 10,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4.84,
+        elevation: 5,
+    },
 })
