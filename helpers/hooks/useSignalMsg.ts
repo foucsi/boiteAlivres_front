bd6b3852a2a86616beee6673cb5850abed96ee8a
Foucsi <julien.foucartreact@gmail.com>
2024-08-14 11:00:09 +0200

    add hook message

--- helpers/hooks/useSignalMsg.ts
+++ helpers/hooks/useSignalMsg.ts
@@ -1,7 +1,9 @@
 import {useMutation} from "react-query";
 import {signalMsg} from "@/helpers/functions/signalMsg";
+import {useSelector} from "react-redux";
 
 export const useSignalMsg = (message:string, email:string) => {
-    const {mutate: sendMsg}=useMutation(()=>signalMsg({message, email}))
+    const user = useSelector((state:any) => state.user.value)
+    const {mutate: sendMsg}=useMutation(()=>signalMsg({message:message, email : user.email}))
     return {sendMsg}
 }
