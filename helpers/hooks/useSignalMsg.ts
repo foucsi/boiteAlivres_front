59a75868c6ebbefd3d45ec67c966eb5c13085ffe
Foucsi <julien.foucartreact@gmail.com>
2024-08-14 14:41:42 +0200

    custom modal send signal msg

--- helpers/hooks/useSignalMsg.ts
+++ helpers/hooks/useSignalMsg.ts
@@ -3,11 +3,13 @@ import {signalMsg} from "@/helpers/functions/signalMsg";
 import {useSelector} from "react-redux";
 
 export const useSignalMsg = (message:string, email:string) => {
-    const {mutate: sendSignalMsg, error}=useMutation(()=>signalMsg({message:message, email : email}), {
+    const {mutate: sendSignalMsg,error}=useMutation(()=>signalMsg({message, email}), {
         onError: (error:unknown) => {
             console.error("Error signal message :", error)
         },
-        onSuccess: ()=>console.log('Signal message')
+        onSuccess: (data)=>{
+            console.log("Result: ",data?.result)
+        }
 
     })
     return {sendSignalMsg, error}
