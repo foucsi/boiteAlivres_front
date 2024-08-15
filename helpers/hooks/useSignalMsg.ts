21aae2903f8af4c7924cd65b6603baf31d830b31
Foucsi <julien.foucartreact@gmail.com>
2024-08-15 15:58:02 +0200

    add function sendMessage

--- helpers/hooks/useSignalMsg.ts
+++ helpers/hooks/useSignalMsg.ts
@@ -2,16 +2,16 @@ import {useMutation} from "react-query";
 import {signalMsg} from "@/helpers/functions/signalMsg";
 import {useSelector} from "react-redux";
 
-export const useSignalMsg = (message:string, email:string,bookPlaceId:string, setShowModal:void, setMessage:void) => {
+export const useSignalMsg = (message:string, email:string,bookPlaceId:string, setShowModal: (value: boolean | ((prevState: boolean) => boolean)) => void,
+                             setMessage: (value: string) => void) => {
     const {mutate: sendSignalMsg,error, data}=useMutation(()=>signalMsg({message, email, bookPlaceId}), {
         onError: (error:unknown) => {
             console.error("Error signal message :", error)
         },
         onSuccess: (data)=>{
             if(data.result){
-                // @ts-ignore
-                setShowModal((prev)=> !prev)
                 setMessage('')
+                setShowModal((prev)=> !prev)
             }
         }
     })
