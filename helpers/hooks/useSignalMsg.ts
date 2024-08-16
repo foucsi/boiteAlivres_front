b6ae9cce301ba719116c3231a9d18036ad8cc73a
Foucsi <julien.foucartreact@gmail.com>
2024-08-16 18:31:35 +0200

    add handleError on useSignalMsg

--- helpers/hooks/useSignalMsg.ts
+++ helpers/hooks/useSignalMsg.ts
@@ -4,10 +4,12 @@ import {useSelector} from "react-redux";
 
 export const useSignalMsg = (message:string, email:string,bookPlaceId:string, setShowModal: (value: boolean | ((prevState: boolean) => boolean)) => void,
                              setMessage: (value: string) => void) => {
+
+    const handleError = (error:unknown) => {
+        console.error("Error signal message :", error)
+    }
     const {mutate: sendSignalMsg,error, data}=useMutation(()=>signalMsg({message, email, bookPlaceId}), {
-        onError: (error:unknown) => {
-            console.error("Error signal message :", error)
-        },
+        onError: handleError,
         onSuccess: (data)=>{
             if(data.result){
                 setMessage('')
