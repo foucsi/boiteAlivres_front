11d7b7c8f7e3bb00ad9050e9a95e0cfa92d2d337
Foucsi <julien.foucartreact@gmail.com>
2024-08-24 18:32:54 +0200

    remove try catch on function addMarkerInDb

--- helpers/functions/addMarkerInDb.ts
+++ helpers/functions/addMarkerInDb.ts
@@ -8,7 +8,6 @@ export const addMarkerInDb = async(uniqueId:string, latitude:number, longitude:n
         postalCode: 'test',
     }
     const url = URL_ADD_MARKER_IN_DB(uniqueId);
-    try{
         const response = await fetch(url, {
             method: 'POST',
             headers: {
@@ -24,14 +23,12 @@ export const addMarkerInDb = async(uniqueId:string, latitude:number, longitude:n
         if(!response.ok){
             throw new Error(`HTTP error! status: ${response.status} ${response.statusText}`);
         }
-        const data = await response.json();
-        if (data.result){
-            return {success: true, data: data.bookPlace}
-        }else {
-            return {success: false, error: data.error}
-        }
+        return await response.json();
+        // const data = await response.json();
+        // if (data.result){
+        //     return {success: true, data: data.bookPlace}
+        // }else {
+        //     return {success: false, error: data.error}
+        // }
 
-    }catch(err){
-        return {success: false, error:err}
-    }
 }
