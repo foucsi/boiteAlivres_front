e9bb3bce6f6338900a8f055cce5498b6fc3e5bda
Foucsi <julien.foucartreact@gmail.com>
2024-08-24 18:39:40 +0200

    add useMutation on iuseAddMarkerIndb function

--- helpers/hooks/useAddMarkerInDb.ts
+++ helpers/hooks/useAddMarkerInDb.ts
@@ -1,31 +1,56 @@
-import {useState} from "react";
-import {addMarkerInDb} from "@/helpers/functions/addMarkerInDb";
-import {useDispatch, useSelector} from "react-redux";
-import {addBookSpace} from "@/redux/bookSpacesSlice"
+import { useState } from "react";
+import { useMutation } from "@tanstack/react-query";
+import { addMarkerInDb } from "@/helpers/functions/addMarkerInDb";
+import { useDispatch, useSelector } from "react-redux";
+import { addBookSpace } from "@/redux/bookSpacesSlice";
 
 export const useAddMarkerInDb = () => {
-    const [modalIsVisible, setModalIsVisible] = useState(false)
-    const [modalPremiumIsVisible, setModalPremiumIsVisible] = useState(false)
+    const [modalIsVisible, setModalIsVisible] = useState(false);
+    const [modalPremiumIsVisible, setModalPremiumIsVisible] = useState(false);
     const dispatch = useDispatch();
-    // @ts-ignore
     const user = useSelector((state) => state.user.value);
-    const addMarker = async(e:any)=>{
-        const {latitude, longitude} = e.nativeEvent.coordinate
-        e.persist()
-        if(user.premium){
-            const result = await addMarkerInDb(user.uniqueId, latitude, longitude, "Découvrez et partagez des livres gratuitement dans cette boîte à livres conviviale.")
-            // console.log("NativeEvent: ", e.nativeEvent.coordinate)
-            if(result.success){
-                setModalIsVisible(true)
-                dispatch(addBookSpace(result.data))
-            }else{
-                console.log("error add marker: ", result)
-            }
-        }else{
-            setModalPremiumIsVisible(true)
-            console.log("Vous devez être premium pour ajouter un marqueur")
-        }
 
-    }
-    return {addMarker, modalIsVisible, setModalIsVisible, modalPremiumIsVisible, setModalPremiumIsVisible};
-}
+    // Utilisation de useMutation pour ajouter un marqueur
+    const { mutate: addMarker } = useMutation(
+        async ({ latitude, longitude }) => {
+            return await addMarkerInDb(
+                user.uniqueId,
+                latitude,
+                longitude,
+                "Découvrez et partagez des livres gratuitement dans cette boîte à livres conviviale."
+            );
+        },
+        {
+            onSuccess: (data) => {
+                if (data.result) {
+                    setModalIsVisible(true);
+                    dispatch(addBookSpace(data.bookPlace));
+                } else {
+                    console.log("Erreur lors de l'ajout du marqueur :", result);
+                }
+            },
+            onError: (error) => {
+                console.error("Erreur lors de la mutation :", error);
+            },
+        }
+    );
+
+    const handleAddMarker = (e) => {
+        const { latitude, longitude } = e.nativeEvent.coordinate;
+
+        if (user.premium) {
+            addMarker({ latitude, longitude });
+        } else {
+            setModalPremiumIsVisible(true);
+            console.log("Vous devez être premium pour ajouter un marqueur");
+        }
+    };
+
+    return {
+        handleAddMarker,
+        modalIsVisible,
+        setModalIsVisible,
+        modalPremiumIsVisible,
+        setModalPremiumIsVisible,
+    };
+};
